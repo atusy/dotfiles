39c962c935d773648432212cb5a39c23d990b6b2
atusy <30277794+atusy@users.noreply.github.com>
2022-03-13 23:01:47 +0900

    update wintabs config to remove dupes and invert wintabs_next and previous keybindings

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -83,19 +83,13 @@ vim.api.nvim_set_keymap('n', '<C-F>', ':Fern . -drawer<CR>', {})
 
 -- wintabs SETTINGS
 for k, v in pairs({
-  ['<C-H>'] = '<Plug>(wintabs_next)',
-  ['<C-L>'] = '<Plug>(wintabs_previous)',
-  ['<C-L>'] = '<Plug>(wintabs_previous)',
+  ['<C-L>'] = '<Plug>(wintabs_next)',
+  ['<C-H>'] = '<Plug>(wintabs_previous)',
   ['<C-T>c'] = '<Plug>(wintabs_close)',
   ['<C-T>u'] = '<Plug>(wintabs_undo)',
   ['<C-T>o'] = '<Plug>(wintabs_only)',
   ['<C-W>c'] = '<Plug>(wintabs_close_window)',
   ['<C-W>o'] = '<Plug>(wintabs_only_window)',
-  ['<C-T>c'] = '<Plug>(wintabs_close)',
-  ['<C-T>u'] = '<Plug>(wintabs_undo)',
-  ['<C-T>o'] = '<Plug>(wintabs_only)',
-  ['<C-W>c'] = '<Plug>(wintabs_close_window)',
-  ['<C-W>o'] = '<Plug>(wintabs_only_window)'
 }) do
   vim.api.nvim_set_keymap('', k, v, {})
 end
