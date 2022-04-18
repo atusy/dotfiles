c5d22c3e8918e2b7739b33901cdc5038b49b0c89
atusy <30277794+atusy@users.noreply.github.com>
2022-04-18 22:20:23 +0900

    refactor

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -76,15 +76,12 @@ vim.api.nvim_create_user_command(
 
 -- PLUGIN SETTINGS
 if vim.fn.empty(vim.fn.glob(vim.fn.stdpath('data') .. '/site/autoload/jetpack.vim')) == 1 then
-vim.api.nvim_exec(
-  [[
+  vim.api.nvim_exec([[
     let jetpack = stdpath('data') . '/site/autoload/jetpack.vim'
     autocmd VimEnter * JetpackSync | source $MYVIMRC
     silent execute '!curl -fLo '.jetpack.' --create-dirs  https://raw.githubusercontent.com/tani/vim-jetpack/master/autoload/jetpack.vim'
     silent execute '!curl -fLo "$HOME/.config/nvim/lua/jetpack.lua" --create-dirs https://raw.githubusercontent.com/tani/vim-jetpack/master/lua/jetpack.lua'
-  ]],
-  false
-)
+  ]], false)
 end
 require('jetpack').startup(function(use)
   -- used also in VS Code
