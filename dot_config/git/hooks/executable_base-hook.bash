0f44c89e8f67234ae355719995a74cae68a98e8d
atusy <30277794+atusy@users.noreply.github.com>
2022-05-03 00:33:18 +0900

    refactor

--- dot_config/git/hooks/executable_base-hook.bash
+++ dot_config/git/hooks/executable_base-hook.bash
@@ -4,25 +4,33 @@ readonly HOOKDIR="$( cd "$( dirname "$0" )" && pwd )"
 readonly HOOKNAME="$1"
 shift 1
 
-GIT_HOOK_VERBOSE="${GIT_HOOK_VERBOSE:-0}"
-
-
-# Skip if requested by ENV var (e.g., SKIP_POST_COMMIT=1)
-SKIP_VARNAME="$(
+readonly GIT_HOOK_VERBOSE="${GIT_HOOK_VERBOSE:-0}"
+readonly SKIP_GIT_HOOKS="${SKIP_GIT_HOOKS:-0}"
+readonly SKIP_VARNAME="$(
   echo "SKIP_${HOOKNAME^^}" | sed -e 's/-/_/g'
 )"
-SKIP_HOOK="$(
-  eval "echo \"\${${SKIP_VARNAME}:-0}\""
-)"
-if [[ $SKIP_HOOK -eq 1 ]]
+
+# Skip if requested by ENV var (e.g., SKIP_POST_COMMIT=1)
+if [[ "$SKIP_GIT_HOOKS" -eq 1 ]] \
+  || [[ "$( eval "echo \"\${${SKIP_VARNAME}:-0}\"" )" -eq 1 ]]
 then
   exit 0
 fi
 
 
-# Run hooks but without recursing git hooks
-function nonrecursive() {
-  eval "${SKIP_VARNAME}=1" "$@"
+# Functions
+function hook() {
+  # verbose message
+  if [[ $GIT_HOOK_VERBOSE -eq 1 ]]
+  then
+    echo "${HOOKNAME}: ${1}" 1>&2
+  fi
+
+  # evaluate executable hook without recursing
+  if [[ -x "$1" ]]
+  then
+    eval "${SKIP_VARNAME}=1" "$@"
+  fi
 }
 
 
@@ -32,21 +40,11 @@ then
   command find "${HOOKDIR}/${HOOKNAME}-files" -type f -name '*.bash' \
     | while read -r fname
   do
-    if [[ -x "${fname}" ]]
-    then
-      [[ $GIT_HOOK_VERBOSE -eq 1 ]] && echo "${HOOKNAME}: $fname" 1>&2
-      nonrecursive "${fname}" "$@"
-    fi
+    hook "${fname}" "$@"
   done
 fi
 
 
 ## Run local hooks
-readonly GIT_DIR="$( git rev-parse --git-common-dir )"
-readonly LOCAL_HOOK="${GIT_DIR}/.git/hooks/${HOOKNAME}"
-if [[ -x "${LOCAL_HOOK}" ]]
-then
-  [[ $GIT_HOOK_VERBOSE -eq 1 ]] && echo "${HOOKNAME}: ${LOCAL_HOOK}" 1>&2
-  nonrecursive "${LOCAL_HOOK}" "$@"
-fi
+hook "$( git rev-parse --git-common-dir )/.git/hooks/${HOOKNAME}" "$@"
 
