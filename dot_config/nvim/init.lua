a5e5dc4382b49bdf6ec18ccc352662c0ff65d5e0
atusy <30277794+atusy@users.noreply.github.com>
2022-08-08 10:24:48 +0900

    let prefix_emoji function be local

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -705,7 +705,7 @@ end
 
 local regex_emoji = '[' .. [[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F1E0-\U0001F1FF\U00002500-\U00002BEF\U00002702-\U000027B0\U00002702-\U000027B0\U0001f926-\U0001f937\U00010000-\U0010ffff\u2640-\u2642\u2600-\u2B55\u200d\u23cf\u23e9\u231a\ufe0f\u3030]] .. ']'
 
-prefix_emoji = function(bufnr, alt)
+local prefix_emoji = function(bufnr, alt)
   bufnr = bufnr or 0
   alt = alt or {'.gitmessage'}
   local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
