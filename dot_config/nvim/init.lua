305dc6e604914f3a93c0bbd88a806c809069933f
atusy <30277794+atusy@users.noreply.github.com>
2022-09-13 14:17:26 +0900

    fix jetpack#sync should be called iff some pakcage is not installed

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -243,7 +243,7 @@ require'jetpack'.startup(function(use)
   }})
 end)
 for _, name in ipairs(vim.fn['jetpack#names']()) do
-  if vim.fn['jetpack#tap'](name) then
+  if not vim.fn['jetpack#tap'](name) then
     vim.fn['jetpack#sync']()
     break
   end
