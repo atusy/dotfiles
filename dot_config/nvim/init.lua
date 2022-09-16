e10c818e5a605f2b5bb93f9dca07e5ecf6d131a1
atusy <30277794+atusy@users.noreply.github.com>
2022-09-17 01:02:27 +0900

    try blinking guicursor

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -68,11 +68,11 @@ vim.opt.listchars = {
   extends = "❯",
   precedes = "❮",
 }
--- vim.opt.guicursor = {
---   [[n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50]],
---   [[a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor]],
---   [[sm:block-blinkwait175-blinkoff150-blinkon175]],
--- }
+vim.opt.guicursor = {
+  [[n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50]],
+  [[a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor]],
+  [[sm:block-blinkwait175-blinkoff150-blinkon175]],
+}
 
 -- search
 vim.opt.hlsearch = true
