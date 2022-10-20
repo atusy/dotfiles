c7e0c8f1446f13f9fc6150881dd6e848063e5a25
atusy <30277794+atusy@users.noreply.github.com>
2022-10-20 14:24:45 +0900

    support r chunk

--- dot_config/nvim/after/queries/markdown/injections.scm
+++ dot_config/nvim/after/queries/markdown/injections.scm
@@ -6,3 +6,10 @@
  (#vim-match? @lang "^(py|python)(:.*)?$")
  (#set! language "python")
 )
+(fenced_code_block
+ (info_string
+   (language) @lang)
+ (code_fence_content) @content
+ (#vim-match? @lang "^r,$")
+ (#set! language "r")
+)
