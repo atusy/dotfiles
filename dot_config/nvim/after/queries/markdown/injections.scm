0a7ad10594c4e47b6749fe6bb1fa5063c4ce8a8b
atusy <30277794+atusy@users.noreply.github.com>
2022-10-20 14:24:21 +0900

    extract language from info_string, not whole info_string

--- dot_config/nvim/after/queries/markdown/injections.scm
+++ dot_config/nvim/after/queries/markdown/injections.scm
@@ -1,5 +1,7 @@
+;; extends
 (fenced_code_block
- (info_string) @lang
+ (info_string
+   (language) @lang)
  (code_fence_content) @content
  (#vim-match? @lang "^(py|python)(:.*)?$")
  (#set! language "python")
