7e6abc8f3d420977830ac05610996458f59d1b82
atusy <30277794+atusy@users.noreply.github.com>
2022-10-29 00:09:37 +0900

    format

--- dot_config/nvim/lua/config/git.lua
+++ dot_config/nvim/lua/config/git.lua
@@ -7,7 +7,7 @@ local set_keymap = utils.set_keymap
 
 -- vgit
 local function setup_vgit()
-  local Vgit = require'vgit'
+  local Vgit = require 'vgit'
   Vgit.setup {
     keymaps = {},
     settings = {
@@ -58,15 +58,60 @@ local function setup_vgit()
   set_keymap('n', '<Plug>(C-G)<C-A>', '<Plug>(vgit.buffer_hunk_stage)')
 end
 
+local setup_gitsigns = function()
+  local gs = require('gitsigns')
+  gs.setup({
+    signcolumn = false,
+    numhl = true,
+    current_line_blame_opts = {
+      delay = 150
+    },
+    on_attach = function(bufnr)
+      local OPTS = { buffer = bufnr }
+      set_keymap({ 'n', 'v' }, '<Plug>(C-G)<C-A>', gs.stage_hunk, OPTS)
+      set_keymap('n', '<Plug>(C-G)a', gs.stage_buffer, OPTS)
+      set_keymap({ 'n', 'v' }, '<Plug>(C-G)<C-R>', gs.reset_hunk, OPTS)
+      set_keymap('n', '<Plug>(C-G)r', gs.reset_buffer, OPTS)
+      set_keymap(
+        'n', '<Plug>(toggle-live-git-blame)',
+        function()
+          vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", { link = "Comment" })
+          gs.toggle_current_line_blame()
+        end,
+        OPTS
+      )
+
+      set_keymap(
+        'n', '<Down>',
+        function()
+          if vim.wo.diff then return ']c' end
+          vim.schedule(function() gs.next_hunk() end)
+          return '<Ignore>'
+        end,
+        { expr = true }
+      )
+
+      set_keymap(
+        'n', '<Up>',
+        function()
+          if vim.wo.diff then return '[c' end
+          vim.schedule(function() gs.prev_hunk() end)
+          return '<Ignore>'
+        end,
+        { expr = true }
+      )
+    end
+  })
+end
 -- gin & gintonic
 local function setup_gin()
   local has_delta = vim.fn.executable('delta') == 1
   if has_delta then
-    vim.g.gin_diff_default_args = {"++processor=delta"}
+    vim.g.gin_diff_default_args = { "++processor=delta" }
   end
   _require("gintonic").setup({
     params = {
-      GinBuffer = {processor = has_delta and "delta" or nil}
+      GinBuffer = { processor = has_delta and "delta" or nil }
     }
   })
   local augroup_gin = api.nvim_create_augroup("gin-custom", {})
@@ -76,8 +121,8 @@ local function setup_gin()
       group = augroup_gin,
       pattern = "gin-patch",
       callback = function()
-        set_keymap("n", "<Plug>(C-G)<C-A>", "<Plug>(gin-diffget-r)", {bufnr = 0}) -- git add
-        set_keymap("n", "<Plug>(C-G)<C-R>", "<Plug>(gin-diffget-l)", {bufnr = 0}) -- git reset
+        set_keymap("n", "<Plug>(C-G)<C-A>", "<Plug>(gin-diffget-r)", { bufnr = 0 }) -- git add
+        set_keymap("n", "<Plug>(C-G)<C-R>", "<Plug>(gin-diffget-l)", { bufnr = 0 }) -- git reset
       end
     }
   )
@@ -108,17 +153,18 @@ end
 -- return
 return {
   deps = {
-    {'lambdalisue/gin.vim'},
-    {'tpope/vim-fugitive'},
-    {'knsh14/vim-github-link'},
-    {'tanvirtin/vgit.nvim'},
-    {'nvim-lua/plenary.nvim'}, -- for vgit
-    {'vim-denops/denops.vim'}, -- for gin
-    -- {'lewis6991/gitsigns.nvim'},
+    { 'lambdalisue/gin.vim' },
+    { 'tpope/vim-fugitive' },
+    { 'knsh14/vim-github-link' },
+    { 'tanvirtin/vgit.nvim' },
+    { 'nvim-lua/plenary.nvim' }, -- for vgit
+    { 'vim-denops/denops.vim' }, -- for gin
+    { 'lewis6991/gitsigns.nvim' },
   },
   setup = function(_)
-    setup_vgit()
+    -- setup_vgit()
     setup_gin()
+    setup_gitsigns()
     -- require('gitsigns').setup({signcolumn = false, numhl = true})
 
     -- fugitive
