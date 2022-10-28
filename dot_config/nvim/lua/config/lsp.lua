987ebaa730c1f05100d8819d705c107f0cb76f82
atusy <30277794+atusy@users.noreply.github.com>
2022-10-28 21:30:30 +0900

    suprress lsp warning

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -76,6 +76,7 @@ local function setup(_)
   -- Use an on_attach function to only map the following keys
   -- after the language server attaches to the current buffer
   local on_attach = function(client, bufnr)
+    local _ = client
     -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
