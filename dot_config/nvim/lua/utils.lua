cd8414647153b2756737d0f50e79f73134682ad1
atusy <30277794+atusy@users.noreply.github.com>
2022-10-29 01:39:53 +0900

    attach_lsp

--- dot_config/nvim/lua/utils.lua
+++ dot_config/nvim/lua/utils.lua
@@ -34,4 +34,20 @@ function M.has_lsp_client(bufnr)
   return false
 end
 
+M.attach_lsp = function(filetype)
+  filetype = filetype or vim.api.nvim_buf_get_option(0, "filetype")
+  local clients = {}
+  for _, cl in ipairs(vim.lsp.get_active_clients()) do
+    if cl.config and cl.config.filetypes then
+      for _, ft in ipairs(cl.config.filetypes) do
+        if ft == filetype then
+          vim.lsp.buf_attach_client(0, cl.id)
+          table.insert(clients, cl)
+        end
+      end
+    end
+  end
+  return clients
+end
+
 return M
