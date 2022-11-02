9fad56e621d902919bc9997247dc72748a893950
atusy <30277794+atusy@users.noreply.github.com>
2022-11-02 20:22:58 +0900

    simplify

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -71,11 +71,9 @@ local function setup_null_ls()
   null_ls.setup({ sources = { gitshow } })
 end
 
-local function resolve_capability(client, feature)
-  return client and client.server_capabilities and client.server_capabilities[feature] or false
-end
-
 local on_attach = function(client, bufnr)
+  local CAPABILITIES = client.server_capabilities
+
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
@@ -95,7 +93,7 @@ local on_attach = function(client, bufnr)
     { desc = 'lsp show workspace folders' })
   set_keymap('n', '<Leader>D', vim.lsp.buf.type_definition, OPTS, { desc = 'lsp type definition' })
   -- set_keymap('n', '<Leader>rn', '<cmd>Lspsaga rename<cr>', OPTS)
-  if resolve_capability(client, "renameProvider") then
+  if CAPABILITIES["renameProvider"] then
     set_keymap('n', '<Leader>rn', vim.lsp.buf.rename, OPTS, { desc = 'lsp rename' })
   end
   set_keymap({ 'n', 'v' }, '<Leader>ca', '<cmd>Lspsaga code_action<cr>', OPTS, { desc = 'lsp code action' })
