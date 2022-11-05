14f085b3887a44520deb6e2d0bfe202e57075c9e
atusy <30277794+atusy@users.noreply.github.com>
2022-11-06 00:31:54 +0900

    lint

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,20 +1,4 @@
 local wezterm = require("wezterm")
-wezterm.on("trigger-nvim-with-scrollback", function(window, pane)
-  local scrollback = pane:get_lines_as_text()
-  local name = os.tmpname()
-  local f = io.open(name, "w+")
-  f:write(scrollback)
-  f:flush()
-  f:close()
-  window:perform_action(
-    wezterm.action({SpawnCommandInNewTab = {
-      args = {"nvim", name},
-    }}),
-    pane
-  )
-  wezterm.sleep_ms(1000)
-  os.remove(name)
-end)
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
   local pane_title = tab.active_pane.title
@@ -27,30 +11,30 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
   return {
     --{Background={Color="black"}},
     --{Foreground={Color="white"}},
-    {Text=" " .. pane_title .. " "},
+    { Text = " " .. pane_title .. " " },
   }
 end)
 
 return {
-  default_prog = {"/usr/bin/env", "zsh"},
+  default_prog = { "/usr/bin/env", "zsh" },
   font = wezterm.font_with_fallback({
     {family = "UDEV Gothic NF"},
     {family = "Noto Color Emoji"},
   }),
   font_size = 12,
   keys = {
-    {key = "e", mods = "ALT|CTRL", action = wezterm.action {EmitEvent = "trigger-nvim-with-scrollback"}},
-    {key = "v", mods = "ALT|CTRL", action = wezterm.action {SplitHorizontal = {domain = "CurrentPaneDomain"}}},
-    {key = "s", mods = "ALT|CTRL", action = wezterm.action {SplitVertical = {domain = "CurrentPaneDomain"}}},
-    {key = "w", mods = "ALT|CTRL", action = wezterm.action {CloseCurrentPane = {confirm = true}}},
-    {key = "h", mods = "ALT|CTRL",
-      action = wezterm.action{ActivatePaneDirection = "Left"}},
-    {key = "l", mods = "ALT|CTRL",
-      action = wezterm.action{ActivatePaneDirection = "Right"}},
-    {key = "k", mods = "ALT|CTRL",
-      action = wezterm.action{ActivatePaneDirection = "Up"}},
-    {key = "j", mods = "ALT|CTRL",
-      action = wezterm.action{ActivatePaneDirection = "Down"}},
+    { key = "e", mods = "ALT|CTRL", action = wezterm.action { EmitEvent = "trigger-nvim-with-scrollback" } },
+    { key = "v", mods = "ALT|CTRL", action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
+    { key = "s", mods = "ALT|CTRL", action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
+    { key = "w", mods = "ALT|CTRL", action = wezterm.action { CloseCurrentPane = { confirm = true } } },
+    { key = "h", mods = "ALT|CTRL",
+      action = wezterm.action { ActivatePaneDirection = "Left" } },
+    { key = "l", mods = "ALT|CTRL",
+      action = wezterm.action { ActivatePaneDirection = "Right" } },
+    { key = "k", mods = "ALT|CTRL",
+      action = wezterm.action { ActivatePaneDirection = "Up" } },
+    { key = "j", mods = "ALT|CTRL",
+      action = wezterm.action { ActivatePaneDirection = "Down" } },
   },
   color_scheme = "Afterglow",
 }
