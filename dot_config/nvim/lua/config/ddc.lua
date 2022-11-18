09153d5ff9677f31a2b2804b94dd46b315eac44f
atusy <30277794+atusy@users.noreply.github.com>
2022-11-18 23:58:55 +0900

    remove comment

--- dot_config/nvim/lua/config/ddc.lua
+++ dot_config/nvim/lua/config/ddc.lua
@@ -96,12 +96,6 @@ local function setup()
     },
   })
   patch_global('completionMenu', 'pum.vim')
-  --[[
-  inoremap <silent><expr> <TAB>
-        \ pum#visible() ? '<Cmd>call pum#map#insert_relative(+1)<CR>' :
-        \ (col('.') <= 1 <Bar><Bar> getline('.')[col('.') - 2] =~# '\s') ?
-        \ '<TAB>' : ddc#manual_complete()
-  --]]
   for lhs, rhs in pairs(maps) do
     set_keymap('i', lhs, rhs, { silent = true, expr = true })
   end
