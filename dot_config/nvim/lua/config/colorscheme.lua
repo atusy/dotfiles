2364515dfe97fc02afe2538ca0e599b825770dac
atusy <30277794+atusy@users.noreply.github.com>
2022-11-22 10:31:21 +0900

    style

--- dot_config/nvim/lua/config/colorscheme.lua
+++ dot_config/nvim/lua/config/colorscheme.lua
@@ -3,7 +3,7 @@ local api = vim.api
 local DEFAULT_COLORSCHEME = 'hatsunemiku'
 local OUTSIDE_COLORSCHEME = 'gruvbox'
 local TAB_COLORSCHEME = 'everforest'
-local ILLUMINATION = {bg="#383D47"}
+local ILLUMINATION = { bg = "#383D47" }
 
 -- set colorscheme
 local function hl_treesitter()
@@ -15,109 +15,109 @@ local function hl_treesitter()
   end
 
   -- Misc {{{
-  hl('@comment', {link = 'Comment'})
+  hl('@comment', { link = 'Comment' })
   -- hl('@error', {link = 'Error'})
-  hl('@none', {bg = 'NONE', fg = 'NONE'})
-  hl('@preproc', {link = 'PreProc'})
-  hl('@define', {link = 'Define'})
-  hl('@operator', {link = 'Operator'})
+  hl('@none', { bg = 'NONE', fg = 'NONE' })
+  hl('@preproc', { link = 'PreProc' })
+  hl('@define', { link = 'Define' })
+  hl('@operator', { link = 'Operator' })
   -- }}}
 
   -- Punctuation {{{
-  hl('@punctuation.delimiter', {link = 'Delimiter'})
-  hl('@punctuation.bracket', {link = 'Delimiter'})
-  hl('@punctuation.special', {link = 'Delimiter'})
+  hl('@punctuation.delimiter', { link = 'Delimiter' })
+  hl('@punctuation.bracket', { link = 'Delimiter' })
+  hl('@punctuation.special', { link = 'Delimiter' })
   -- }}}
 
   -- Literals {{{
-  hl('@string', {link = 'String'})
-  hl('@string.regex', {link = 'String'})
-  hl('@string.escape', {link = 'SpecialChar'})
-  hl('@string.special', {link = 'SpecialChar'})
+  hl('@string', { link = 'String' })
+  hl('@string.regex', { link = 'String' })
+  hl('@string.escape', { link = 'SpecialChar' })
+  hl('@string.special', { link = 'SpecialChar' })
 
-  hl('@character', {link = 'Character'})
-  hl('@character.special', {link = 'SpecialChar'})
+  hl('@character', { link = 'Character' })
+  hl('@character.special', { link = 'SpecialChar' })
 
-  hl('@boolean', {link = 'Boolean'})
-  hl('@number', {link = 'Number'})
-  hl('@float', {link = 'Float'})
+  hl('@boolean', { link = 'Boolean' })
+  hl('@number', { link = 'Number' })
+  hl('@float', { link = 'Float' })
   -- }}}
 
   -- Functions {{{
-  hl('@function', {link = 'Function'})
-  hl('@function.call', {link = 'Function'})
-  hl('@function.builtin', {link = 'Special'})
-  hl('@function.macro', {link = 'Macro'})
+  hl('@function', { link = 'Function' })
+  hl('@function.call', { link = 'Function' })
+  hl('@function.builtin', { link = 'Special' })
+  hl('@function.macro', { link = 'Macro' })
 
-  hl('@method', {link = 'Function'})
-  hl('@method.call', {link = 'Function'})
+  hl('@method', { link = 'Function' })
+  hl('@method.call', { link = 'Function' })
 
-  hl('@constructor', {link = 'Special'})
-  hl('@parameter', {link = 'Identifier'})
+  hl('@constructor', { link = 'Special' })
+  hl('@parameter', { link = 'Identifier' })
   -- }}}
 
   -- Keywords {{{
-  hl('@keyword', {link = 'Keyword'})
-  hl('@keyword.function', {link = 'Keyword'})
-  hl('@keyword.operator', {link = 'Keyword'})
-  hl('@keyword.return', {link = 'Keyword'})
+  hl('@keyword', { link = 'Keyword' })
+  hl('@keyword.function', { link = 'Keyword' })
+  hl('@keyword.operator', { link = 'Keyword' })
+  hl('@keyword.return', { link = 'Keyword' })
 
-  hl('@conditional', {link = 'Conditional'})
-  hl('@repeat', {link = 'Repeat'})
-  hl('@debug', {link = 'Debug'})
-  hl('@label', {link = 'Label'})
-  hl('@include', {link = 'Include'})
-  hl('@exception', {link = 'Exception'})
+  hl('@conditional', { link = 'Conditional' })
+  hl('@repeat', { link = 'Repeat' })
+  hl('@debug', { link = 'Debug' })
+  hl('@label', { link = 'Label' })
+  hl('@include', { link = 'Include' })
+  hl('@exception', { link = 'Exception' })
   -- }}}
 
   -- Types {{{
-  hl('@type', {link = 'Type'})
-  hl('@type.builtin', {link = 'Type'})
-  hl('@type.qualifier', {link = 'Type'})
-  hl('@type.definition', {link = 'Typedef'})
+  hl('@type', { link = 'Type' })
+  hl('@type.builtin', { link = 'Type' })
+  hl('@type.qualifier', { link = 'Type' })
+  hl('@type.definition', { link = 'Typedef' })
 
-  hl('@storageclass', {link = 'StorageClass'})
-  hl('@attribute', {link = 'PreProc'})
-  hl('@field', {link = 'Identifier'})
-  hl('@property', {link = 'Function'})
+  hl('@storageclass', { link = 'StorageClass' })
+  hl('@attribute', { link = 'PreProc' })
+  hl('@field', { link = 'Identifier' })
+  hl('@property', { link = 'Function' })
   -- }}}
 
   -- Identifiers {{{
-  hl('@variable', {link = 'Normal'})
-  hl('@variable.builtin', {link = 'Special'})
+  hl('@variable', { link = 'Normal' })
+  hl('@variable.builtin', { link = 'Special' })
 
-  hl('@constant', {link = 'Constant'})
-  hl('@constant.builtin', {link = 'Special'})
-  hl('@constant.macro', {link = 'Define'})
+  hl('@constant', { link = 'Constant' })
+  hl('@constant.builtin', { link = 'Special' })
+  hl('@constant.macro', { link = 'Define' })
 
-  hl('@namespace', {link = 'Include'})
-  hl('@symbol', {link = 'Identifier'})
+  hl('@namespace', { link = 'Include' })
+  hl('@symbol', { link = 'Identifier' })
   -- }}}
 
   -- Text {{{
-  hl('@text', {link = 'Normal'})
-  hl('@text.strong', {bold = true})
-  hl('@text.emphasis', {italic = true})
-  hl('@text.underline', {underline = true})
-  hl('@text.strike', {strikethrough = true})
-  hl('@text.title', {link = 'Title'})
-  hl('@text.literal', {link = 'String'})
-  hl('@text.uri', {link = 'Underlined'})
-  hl('@text.math', {link = 'Special'})
-  hl('@text.environment', {link = 'Macro'})
-  hl('@text.environment.name', {link = 'Type'})
-  hl('@text.reference', {link = 'Constant'})
+  hl('@text', { link = 'Normal' })
+  hl('@text.strong', { bold = true })
+  hl('@text.emphasis', { italic = true })
+  hl('@text.underline', { underline = true })
+  hl('@text.strike', { strikethrough = true })
+  hl('@text.title', { link = 'Title' })
+  hl('@text.literal', { link = 'String' })
+  hl('@text.uri', { link = 'Underlined' })
+  hl('@text.math', { link = 'Special' })
+  hl('@text.environment', { link = 'Macro' })
+  hl('@text.environment.name', { link = 'Type' })
+  hl('@text.reference', { link = 'Constant' })
 
-  hl('@text.todo', {link = 'Todo'})
-  hl('@text.note', {link = 'SpecialComment'})
-  hl('@text.warning', {link = 'WarningMsg'})
-  hl('@text.danger', {link = 'ErrorMsg'})
+  hl('@text.todo', { link = 'Todo' })
+  hl('@text.note', { link = 'SpecialComment' })
+  hl('@text.warning', { link = 'WarningMsg' })
+  hl('@text.danger', { link = 'ErrorMsg' })
   -- }}}
 
   -- Tags {{{
-  hl('@tag', {link = 'Tag'})
-  hl('@tag.attribute', {link = 'Identifier'})
-  hl('@tag.delimiter', {link = 'Delimiter'})
+  hl('@tag', { link = 'Tag' })
+  hl('@tag.attribute', { link = 'Identifier' })
+  hl('@tag.delimiter', { link = 'Delimiter' })
   -- }}}
 
   -- Customs {{{
@@ -154,7 +154,7 @@ local function set_autocmd(opt)
       desc = 'Fix colorscheme if filetype is set after bufenter',
       group = GROUP,
       nested = true,
-      pattern = {'help'},
+      pattern = { 'help' },
       callback = function(args)
         if api.nvim_get_current_tabpage() ~= 1 then
           set_colorscheme(TAB, false, opt)
@@ -180,14 +180,14 @@ local function set_autocmd(opt)
         local BUFTYPE = api.nvim_buf_get_option(0, "buftype")
         local CWD = vim.fn.getcwd()
         local COLORSCHEME = (
-          FILE == '' or
-          FILETYPE == 'gitcommit' or
-          FILETYPE == 'gitrebase' or
-          FILETYPE == 'help' or
-          BUFTYPE ~= '' or
-          CWD == string.sub(FILE, 1, string.len(CWD)) or
-          '/tmp/' == string.sub(FILE, 1, 5)
-        ) and DEFAULT or OUTSIDE
+            FILE == '' or
+                FILETYPE == 'gitcommit' or
+                FILETYPE == 'gitrebase' or
+                FILETYPE == 'help' or
+                BUFTYPE ~= '' or
+                CWD == string.sub(FILE, 1, string.len(CWD)) or
+                '/tmp/' == string.sub(FILE, 1, 5)
+            ) and DEFAULT or OUTSIDE
 
         -- Apply colorscheme and some highlight settings
         set_colorscheme(COLORSCHEME, false, opt)
@@ -199,16 +199,16 @@ end
 -- return
 return {
   deps = {
-    {'4513ECHO/vim-colors-hatsunemiku'},
-    {'morhetz/gruvbox'},
-    {'sainnhe/everforest'},
-    {'m-demare/hlargs.nvim'},
-    {'RRethy/vim-illuminate'},
-    {'norcalli/nvim-colorizer.lua'},
-    {'folke/lsp-colors.nvim'},
+    { '4513ECHO/vim-colors-hatsunemiku' },
+    { 'morhetz/gruvbox' },
+    { 'sainnhe/everforest' },
+    { 'm-demare/hlargs.nvim' },
+    { 'RRethy/vim-illuminate' },
+    { 'norcalli/nvim-colorizer.lua' },
+    { 'folke/lsp-colors.nvim' },
   },
   setup = function(opt)
-    opt = opt or {colorscheme = {}}
+    opt = opt or { colorscheme = {} }
     set_colorscheme(opt.colorscheme.default or DEFAULT_COLORSCHEME, true, opt)
     set_autocmd(opt)
   end
