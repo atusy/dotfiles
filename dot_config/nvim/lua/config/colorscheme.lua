649f0df094f150b3ac428e39e65452cfe1f5cb06
atusy <30277794+atusy@users.noreply.github.com>
2022-11-22 23:24:06 +0900

    update colorscheme

--- dot_config/nvim/lua/config/colorscheme.lua
+++ dot_config/nvim/lua/config/colorscheme.lua
@@ -1,8 +1,8 @@
 -- const
 local api = vim.api
-local DEFAULT_COLORSCHEME = 'hatsunemiku'
-local OUTSIDE_COLORSCHEME = 'gruvbox'
-local TAB_COLORSCHEME = 'everforest'
+local DEFAULT_COLORSCHEME = 'catppuccin'
+local OUTSIDE_COLORSCHEME = 'terafox'
+local TAB_COLORSCHEME = 'carbonfox'
 local ILLUMINATION = { bg = "#383D47" }
 
 -- set colorscheme
@@ -164,6 +164,17 @@ local function set_autocmd(opt)
       end
     }
   )
+  api.nvim_create_autocmd('TabEnter', {
+    group = GROUP,
+    nested = true,
+    callback = function(_)
+      if api.nvim_get_current_tabpage() == 1 then
+        set_colorscheme(DEFAULT, false, opt)
+      else
+        set_colorscheme(TAB, false, opt)
+      end
+    end
+  })
   api.nvim_create_autocmd(
     'BufEnter',
     {
@@ -171,26 +182,24 @@ local function set_autocmd(opt)
       nested = true,
       desc = 'Change theme by the path of the current buffer.',
       callback = function(args)
-        if api.nvim_get_current_tabpage() ~= 1 then
-          set_colorscheme(TAB, false, opt)
-          return
-        end
+        if api.nvim_get_current_tabpage() ~= 1 then return end
         local FILE = args.file
         local FILETYPE = api.nvim_buf_get_option(0, "filetype")
         local BUFTYPE = api.nvim_buf_get_option(0, "buftype")
         local CWD = vim.fn.getcwd()
-        local COLORSCHEME = (
-            FILE == '' or
-                FILETYPE == 'gitcommit' or
-                FILETYPE == 'gitrebase' or
-                FILETYPE == 'help' or
-                BUFTYPE ~= '' or
-                CWD == string.sub(FILE, 1, string.len(CWD)) or
-                '/tmp/' == string.sub(FILE, 1, 5)
-            ) and DEFAULT or OUTSIDE
+        if (FILE == '' or
+            FILETYPE == 'gitcommit' or
+            FILETYPE == 'gitrebase' or
+            FILETYPE == 'help' or
+            BUFTYPE ~= '' or
+            CWD == string.sub(FILE, 1, string.len(CWD)) or
+            '/tmp/' == string.sub(FILE, 1, 5))
+        then
+          return
+        end
 
         -- Apply colorscheme and some highlight settings
-        set_colorscheme(COLORSCHEME, false, opt)
+        vim.cmd("Styler " .. OUTSIDE)
       end
     }
   )
@@ -199,15 +208,20 @@ end
 -- return
 return {
   deps = {
-    { '4513ECHO/vim-colors-hatsunemiku' },
-    { 'morhetz/gruvbox' },
-    { 'sainnhe/everforest' },
+    -- { '4513ECHO/vim-colors-hatsunemiku' },
+    -- { 'ellisonleao/gruvbox.nvim' },
+    -- { 'sainnhe/everforest' },
     { 'm-demare/hlargs.nvim' },
     { 'RRethy/vim-illuminate' },
     { 'norcalli/nvim-colorizer.lua' },
     { 'folke/lsp-colors.nvim' },
+    { 'folke/styler.nvim' },
+    { "catppuccin/nvim", as = "catppuccin" },
+    { "EdenEast/nightfox.nvim" },
+    -- { "RRethy/nvim-base16" },
   },
   setup = function(opt)
+    require('styler').setup({ themes = {} })
     opt = opt or { colorscheme = {} }
     set_colorscheme(opt.colorscheme.default or DEFAULT_COLORSCHEME, true, opt)
     set_autocmd(opt)
