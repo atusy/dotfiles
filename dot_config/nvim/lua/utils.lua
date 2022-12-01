274a0c46f25f00a97a8cd8738ea1f1d6f9b894d5
atusy <30277794+atusy@users.noreply.github.com>
2022-12-01 09:17:10 +0900

    ✨ allow set_keymap() to chose whether or not to add stars

--- dot_config/nvim/lua/utils.lua
+++ dot_config/nvim/lua/utils.lua
@@ -22,11 +22,14 @@ function M.set_keymap(mode, lhs, rhs, opt1, opt2)
   elseif opt1 then
     opt = opt1
   end
-  if opt.desc then
-    opt.desc = '☆' .. opt.desc
-  elseif type(rhs) == "string" then
-    opt.desc = '☆' .. rhs
+  if opt.fav ~= false then
+    if opt.desc then
+      opt.desc = '☆' .. opt.desc
+    elseif type(rhs) == "string" then
+      opt.desc = '☆' .. rhs
+    end
   end
+  opt.fav = nil
   set_keymap(mode, lhs, rhs, opt)
 end
 
