6ac10be9bd273bc6684874c5b4df236ce13ce358
atusy <30277794+atusy@users.noreply.github.com>
2022-12-01 22:53:29 +0900

    🐛 chowcho_hide should not close first tab page

--- dot_config/nvim/lua/config/window.lua
+++ dot_config/nvim/lua/config/window.lua
@@ -1,21 +1,17 @@
 local vim = vim
+local api = vim
+local list_wins = api.nvim_tabpage_list_wins
 
 local safely = require('utils').safely
 local set_keymap = require('utils').set_keymap
 
-local function list_win_config(tabpage)
-  return vim.tbl_map(
-    vim.api.nvim_win_get_config,
-    vim.api.nvim_tabpage_list_wins(tabpage or 0)
-  )
-end
-
-local function is_win_focusable(config)
+local function is_win_focusable(win)
+  local config = api.nvim_win_get_config(win)
   return config.focusable
 end
 
-local function count_win_focusable(tabpage)
-  return #vim.tbl_filter(is_win_focusable, list_win_config(tabpage))
+local function count_win_focusable(wins)
+  return #vim.tbl_filter(is_win_focusable, wins)
 end
 
 local setup = function()
@@ -27,14 +23,13 @@ local setup = function()
 
   local function chowcho_focus()
     -- Focues window
-    if count_win_focusable(0) > 2 then
+    if count_win_focusable(list_wins(0)) > 2 then
       run(
-        safely(vim.api.nvim_set_current_win),
+        safely(api.nvim_set_current_win),
         {
           use_exclude_default = false,
           exclude = function(_, win)
-            local config = vim.api.nvim_win_get_config(win)
-            return config.focusable == false
+            return api.nvim_win_get_config(win).focusable == false
           end
         }
       )
@@ -47,13 +42,13 @@ local setup = function()
   set_keymap({ '', 't' }, '<C-W>w', chowcho_focus)
 
   local function _chowcho_hide()
-    local nwins = #vim.api.nvim_tabpage_list_wins(0)
-    if nwins == 1 and #vim.api.nvim_list_tabpages() > 1 then
+    local nwins = #list_wins(0)
+    if nwins == 1 and api.nvim_get_current_tabpage() ~= 1 then
       vim.cmd("tabclose")
     elseif nwins == 2 then
       vim.cmd("wincmd o")
     elseif nwins > 2 then
-      run(safely(vim.api.nvim_win_hide))
+      run(safely(api.nvim_win_hide))
     end
   end
 
@@ -63,15 +58,15 @@ local setup = function()
 
   local function _chowcho_edit()
     -- Edits buffer from the selected in the current
-    if #vim.api.nvim_tabpage_list_wins(0) < 1 then return end
+    if #list_wins(0) < 1 then return end
     run(
       safely(function(n)
-        vim.api.nvim_win_set_buf(0, vim.api.nvim_win_get_buf(n))
+        api.nvim_win_set_buf(0, api.nvim_win_get_buf(n))
       end),
       {
         use_exclude_default = false,
         exclude = function(_, win)
-          local winconf = vim.api.nvim_win_get_config(win)
+          local winconf = api.nvim_win_get_config(win)
           return winconf.external or winconf.relative ~= ""
         end
       }
@@ -84,23 +79,23 @@ local setup = function()
   local function _chowcho_exchange()
     -- Swaps buffers between windows
     -- also moves between windows to apply styler.nvim theming
-    if #vim.api.nvim_tabpage_list_wins(0) <= 2 then
+    if #list_wins(0) <= 2 then
       vim.cmd("wincmd x")
       return
     end
     run(
       safely(function(n)
-        local cur = vim.api.nvim_get_current_win()
+        local cur = api.nvim_get_current_win()
         if n == cur then return end
-        local bufnr0 = vim.api.nvim_win_get_buf(cur)
-        local bufnrn = vim.api.nvim_win_get_buf(n)
-        vim.api.nvim_win_set_buf(cur, bufnrn)
-        vim.api.nvim_win_set_buf(n, bufnr0)
+        local bufnr0 = api.nvim_win_get_buf(cur)
+        local bufnrn = api.nvim_win_get_buf(n)
+        api.nvim_win_set_buf(cur, bufnrn)
+        api.nvim_win_set_buf(n, bufnr0)
       end),
       {
         use_exclude_default = false,
         exclude = function(_, win)
-          local winconf = vim.api.nvim_win_get_config(win)
+          local winconf = api.nvim_win_get_config(win)
           return winconf.external or winconf.relative ~= ""
         end
       }
