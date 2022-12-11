cb17da091f4b0a241a7cbfb69230c27df0837c78
atusy <30277794+atusy@users.noreply.github.com>
2022-12-12 01:33:59 +0900

    ⚡️ do require'leap-ast' on invoking keymap

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -638,7 +638,7 @@ set_keymap(
 )
 -- set_keymap('o', 'm', ':<C-U>lua require"tsht".nodes()<CR>', {silent = true})
 -- set_keymap('v', 'm', ':lua require"tsht".nodes()<CR>', {silent = true})
-set_keymap({ 'v', 'o' }, 'm', require 'leap-ast'.leap, { silent = true })
+set_keymap({ 'v', 'o' }, 'm', function() require 'leap-ast'.leap() end, { silent = true })
 set_keymap('n', 'zf', function()
   vim.cmd("normal! v")
   require 'leap-ast'.leap()
