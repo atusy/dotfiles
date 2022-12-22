0e58565fa733d527866dceb675db72803b6ff438
atusy <30277794+atusy@users.noreply.github.com>
2022-12-22 10:33:41 +0900

    💥 unsupport reloading MYVIMRC for now

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,8 +1,2 @@
-vim.opt.runtimepath:append(vim.fn.stdpath('data') .. '/impatient')
-local ok, impatient = pcall(require, 'impatient')
-if ok then
-  impatient.enable_profile()
-else
-  vim.notify(ok, vim.log.levels.ERROR)
-end
-require('utils').require('atusy')
+require('atusy')
+-- require('utils').require('atusy')
