ca8f7f81b5632445a4f38518ad99b92cd10ac5b1
atusy <30277794+atusy@users.noreply.github.com>
2022-12-26 22:27:49 +0900

    ✨ map vzf as Vzf

--- dot_config/nvim/lua/atusy.lua
+++ dot_config/nvim/lua/atusy.lua
@@ -181,6 +181,9 @@ set_keymap({ 'n', 'v' }, 'gY', '"+Y')
 set_keymap('c', '<C-A>', '<Home>')
 set_keymap('t', '<C-W>', [[<C-\><C-N><C-W>]])
 set_keymap({ '', '!', 't' }, [[<C-\><C-\>]], [[<C-\><C-N>]])
+set_keymap('x', 'zf', function()
+  return vim.api.nvim_get_mode().mode ~= 'V' and 'Vzf' or 'zf'
+end, { expr = true })
 
 local function jump(forward)
   local buf_cur = vim.api.nvim_get_current_buf()
