9309558a74b348779b33830c3311523700a575d4
atusy <30277794+atusy@users.noreply.github.com>
2023-01-11 17:31:59 +0900

    ✨ more robust fallback for chezmoi managed dot files

--- dot_config/nvim/filetype.lua
+++ dot_config/nvim/filetype.lua
@@ -5,12 +5,18 @@ vim.filetype.add({
     ['.tf'] = 'terraform',
   },
   pattern = {
-    ['dot_.*'] = function(path, bufnr)
-      return vim.filetype.match({
-        filename = vim.fs.basename(path):gsub('^dot_', '.'),
-        buf = bufnr,
-      })
-    end,
+    ['${HOME}/.local/share/chezmoi/.*'] = {
+      function(path, bufnr)
+        if not path:match('/dot_') then return end
+        return vim.filetype.match({
+          filename = path:gsub('/dot_', '/.'),
+          buf = bufnr,
+        })
+      end,
+      {
+        priority = -math.huge,
+      }
+    },
     ['Dockerfile[._].*'] = { 'dockerfile', { priority = -math.huge } }
   }
 })
