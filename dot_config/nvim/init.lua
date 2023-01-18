c11f58f15f4d492d15c81e4fb413ab86c41b6697
atusy <30277794+atusy@users.noreply.github.com>
2023-01-18 21:10:32 +0900

    ✨ refactor by defining atusy.lazy

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -365,16 +365,11 @@ end
 
 --[[ lazy.nvim ]]
 local lazypath = utils.datapath .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--single-branch",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
+  require('atusy.lazy').install(lazypath)
 end
+
 vim.opt.runtimepath:prepend(lazypath)
 require('lazy').setup("plugins", {
   change_detection = { enabled = false },
@@ -395,42 +390,5 @@ require('lazy').setup("plugins", {
 })
 
 if vim.v.vim_did_enter == 1 then
-  --[[ if vim.g['sandwich#default_recipes'] == nil then
-    require('lazy').load({ plugins = 'vim-sandwich' })
-  end ]]
-  local function try(...)
-    local ok, res = pcall(...)
-    if not ok then vim.notify(res, vim.log.levels.ERROR) end
-    return ok
-  end
-
-  local loaded = vim.tbl_keys(package.loaded)
-  for _, p in pairs(loaded) do
-    if p == 'atusy' or vim.startswith(p, 'atusy.') then
-      utils.require(p)
-    end
-  end
-
-  local configurable
-  for _, p in pairs(loaded) do
-    if p == 'plugins' or vim.startswith(p, 'plugins.') then
-      for _, d in pairs(utils.require(p)) do
-        if type(d) == "table" and d.enabled ~= false and d.cond ~= false then
-          local pname = d[1]:gsub('.*/', '')
-          require('lazy').load({ plugins = pname })
-          if d.init then
-            configurable = try(d.init)
-            if not configurable then
-              vim.notify('FAILED: ' .. d .. '.init()', vim.log.levels.ERROR)
-            end
-          end
-          if d.config and configurable then
-            if not try(d.config) then
-              vim.notify('FAILED: ' .. d .. '.config()', vim.log.levels.ERROR)
-            end
-          end
-        end
-      end
-    end
-  end
+  require('utils').require('atusy.lazy').reload()
 end
---
+++ dot_config/nvim/lua/atusy/lazy.lua
@@ -0,0 +1,50 @@
+local M = {}
+
+function M.install(lazypath)
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+
+function M.reload()
+  local function try(...)
+    local ok, res = pcall(...)
+    if not ok then vim.notify(res, vim.log.levels.ERROR) end
+    return ok
+  end
+
+  local loaded = vim.tbl_keys(package.loaded)
+  for _, p in pairs(loaded) do
+    if p == 'atusy' or vim.startswith(p, 'atusy.') then
+      require('utils').require(p)
+    end
+  end
+
+  local configurable
+  for _, p in pairs(loaded) do
+    if p == 'plugins' or vim.startswith(p, 'plugins.') then
+      for _, d in pairs(require('utils').require(p)) do
+        if type(d) == "table" and d.enabled ~= false and d.cond ~= false then
+          local pname = d[1]:gsub('.*/', '')
+          require('lazy').load({ plugins = pname })
+          if d.init then
+            configurable = try(d.init)
+            if not configurable then
+              vim.notify('FAILED: ' .. d .. '.init()', vim.log.levels.ERROR)
+            end
+          end
+          if d.config and configurable then
+            if not try(d.config) then
+              vim.notify('FAILED: ' .. d .. '.config()', vim.log.levels.ERROR)
+            end
+          end
+        end
+      end
+    end
+  end
+end
