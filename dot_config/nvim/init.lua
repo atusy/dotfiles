a4409d6c997e5b9131871a2c4fe8ca8377937c0b
atusy <30277794+atusy@users.noreply.github.com>
2023-01-19 20:48:10 +0900

    🐛 expr mapping should return mappings

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -212,8 +212,8 @@ local function jump(forward)
   end
 end
 
-set_keymap('n', 'g<C-O>', function() jump(false) end, { fav = false, expr = true })
-set_keymap('n', 'g<C-I>', function() jump(true) end, { fav = false, expr = true })
+set_keymap('n', 'g<C-O>', function() return jump(false) end, { fav = false, expr = true })
+set_keymap('n', 'g<C-I>', function() return jump(true) end, { fav = false, expr = true })
 
 -- mappings: save and ...
 set_keymap(
