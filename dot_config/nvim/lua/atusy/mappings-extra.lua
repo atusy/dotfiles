8aae63ca240a9d376c087c36a1c28b7a07d94410
atusy <30277794+atusy@users.noreply.github.com>
2023-01-20 22:00:04 +0900

    ✨ add redraw! to palette

--- dot_config/nvim/lua/atusy/mappings-extra.lua
+++ dot_config/nvim/lua/atusy/mappings-extra.lua
@@ -7,3 +7,7 @@ set_palette('n', 'clipboard abs path of dirname of %', '<Cmd>let @+=expand("%:p:
 
 --[[ treesitter ]]
 set_palette('n', 'treesitter show tree', function() vim.treesitter.show_tree() end)
+set_palette(
+  'n', 'redraw!', [[<Cmd>call setline(1, getline(1, '$'))<CR><Cmd>silent undo<CR><Cmd>redraw!<CR>]],
+  { desc = 'with full rewrite of the current buffer, which may possibly fixes broken highlights by treesitter' }
+)
