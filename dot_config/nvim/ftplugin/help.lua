142e7fb15344caad59a0ce8e390b3e25a7b9c7ab
atusy <30277794+atusy@users.noreply.github.com>
2023-01-21 16:33:37 +0900

    🐛 help not showing on rightest when previously opened content is shown

--- dot_config/nvim/ftplugin/help.lua
+++ dot_config/nvim/ftplugin/help.lua
@@ -1,14 +1,25 @@
 pcall(vim.treesitter.start)
 vim.wo.conceallevel = 0
 
-local wins = vim.api.nvim_tabpage_list_wins(0)
-local wins_help = vim.tbl_filter(
-  function(w)
-    local ft = vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(w), 'filetype')
-    return ft == 'help'
-  end,
-  wins
-)
-if #wins_help == 1 then
-  vim.api.nvim_win_call(wins_help[1], function() vim.cmd('wincmd L') end)
+local function wincmd_L()
+  local wins = vim.api.nvim_tabpage_list_wins(0)
+  local wins_help = vim.tbl_filter(
+    function(w)
+      local ft = vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(w), 'filetype')
+      return ft == 'help'
+    end,
+    wins
+  )
+  if #wins_help == 1 then
+    vim.api.nvim_win_call(wins_help[1], function() vim.cmd('wincmd L') end)
+  end
 end
+
+-- Run on FileType event
+wincmd_L()
+
+-- Run on BufWinEnter
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  buffer = vim.api.nvim_get_current_buf(),
+  callback = wincmd_L
+})
