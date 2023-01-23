cd7661759ed8b245b56e3da2251626256a4b5e1b
atusy <30277794+atusy@users.noreply.github.com>
2023-01-24 06:33:56 +0900

    add commented out implementation of mode-dependent textobj

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -562,6 +562,45 @@ local deps = {
     'echasnovski/mini.ai',
     event = 'ModeChanged',
     config = function()
+      --[[
+      -- TODO:
+      -- Vi[ で [ と ] の間から両端の改行を抜いたところを選択したい
+      local function template_bracket()
+        local mode = vim.api.nvim_get_mode().mode
+        local template = '^.%s().*()%s.$'
+        if mode == 'V' then
+          return string.format(
+            template,
+            '%s -\n?',
+            '\n? -%s' -- '\n -%s' works, but this is not what I want...
+          )
+        end
+        return template
+      end
+
+      local _bracket = template_bracket()
+
+      -- mini.aiのtext objはnormalモードで評価されるので、条件分岐はModeChangedでやっておく
+      vim.api.nvim_create_autocmd('ModeChanged', {
+        pattern = '*:[vV]*',
+        group = require('utils').augroup,
+        callback = function()
+          _bracket = template_bracket()
+        end
+      })
+
+      local function bracket(x, left, right)
+        return function()
+          return {
+            '%b' .. x,
+            string.format(_bracket, left or '', right or ''),
+          }
+        end
+      end
+
+      -- usage:
+      -- custom_textobjects = { ['{'] = bracket('{}'), ['}'] = bracket('{}', '%{', '%}') }
+      ]]
       require('mini.ai').setup({
         n_lines = 100,
         mappings = {
