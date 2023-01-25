c03ff998414a5d7a6c1fa4e16cb03dc52837e630
atusy <30277794+atusy@users.noreply.github.com>
2023-01-25 21:42:22 +0900

    ♻️ integrate surround.lua into textobj.lua

--- dot_config/nvim/lua/plugins/textobj.lua
+++ dot_config/nvim/lua/plugins/textobj.lua
@@ -99,4 +99,118 @@ return {
       })
     end,
   },
+  {
+    'machakann/vim-sandwich',
+    cond = false,
+    keys = { { 's', mode = '' }, },
+    config = function()
+      vim.g['sandwich#recipes'] = vim.deepcopy(vim.g['sandwich#default_recipes'])
+      local recipes = vim.fn.deepcopy(vim.g['operator#sandwich#default_recipes'])
+      table.insert(recipes, {
+        buns = { '(', ')' },
+        kind = { 'add' },
+        action = { 'add' },
+        cursor = 'head',
+        command = { 'startinsert' },
+        input = { vim.api.nvim_replace_termcodes("<C-F>", true, false, true) },
+      })
+      vim.g['operator#sandwich#recipes'] = recipes
+    end
+  },
+  {
+    'echasnovski/mini.surround',
+    keys = { { 's', mode = '' } },
+    config = function()
+      --[=[
+      Examples
+        saiw[ surrounds inner word with [] and saiw] surrounds inner word with [[]]
+        Similar behaviors occurs with (){}<>
+
+        saiwj[ surrounds inner word with 「」
+        srj[j] replaces 「」 with 『』
+      ]=]
+      require('mini.surround').setup({
+        n_lines = 100,
+        mappings = {
+          find = 'st',
+          find_left = 'sT',
+          highlight = 'sH',
+        },
+        custom_surroundings = {
+          ['{'] = {
+            input = { '%b{}', '^.().*().$' },
+            output = { left = '{', right = '}' },
+          },
+          ['}'] = {
+            input = { '%b{}', '^.%{().*()%}.$' },
+            output = { left = '{{', right = '}}' },
+          },
+          ['('] = {
+            input = { '%b()', '^.().*().$' },
+            output = { left = '(', right = ')' },
+          },
+          [')'] = {
+            input = { '%b()', '^.%(().*()%).$' },
+            output = { left = '((', right = '))' },
+          },
+          ['['] = {
+            input = { '%b[]', '^.().*().$' },
+            output = { left = '[', right = ']' },
+          },
+          [']'] = {
+            input = { '%b[]', '^.%[().*()%].$' },
+            output = { left = '[[', right = ']]' },
+          },
+          ['<'] = {
+            input = { '%b<>', '^.().*().$' },
+            output = { left = '<', right = '>' },
+          },
+          ['>'] = {
+            input = { '%b[]', '^.<().*()>.$' },
+            output = { left = '<<', right = '>>' },
+          },
+          ['j'] = {
+            input = function()
+              local ok, val = pcall(vim.fn.getchar)
+              if not ok then return end
+              local char = vim.fn.nr2char(val)
+
+              local dict = {
+                ['('] = { '（().-()）' },
+                ['{'] = { '｛().-()｝' },
+                ['['] = { '「().-()」' },
+                [']'] = { '『().-()』' },
+              }
+
+              if char == 'b' then
+                local ret = {}
+                for _, v in pairs(dict) do table.insert(ret, v) end
+                return { ret }
+              end
+
+              if dict[char] then return dict[char] end
+
+              error(char .. ' is unsupported surroundings in Japanese')
+            end,
+            output = function()
+              local ok, val = pcall(vim.fn.getchar)
+              if not ok then return end
+              local char = vim.fn.nr2char(val)
+
+              local dict = {
+                ['('] = { left = '（', right = '）' },
+                ['{'] = { left = '｛', right = '｝' },
+                ['['] = { left = '「', right = '」' },
+                [']'] = { left = '『', right = '』' },
+              }
+
+              if dict[char] then return dict[char] end
+
+              error(char .. ' is unsupported surroundings in Japanese')
+            end
+          }
+        }
+      })
+    end,
+  },
 }
