128c88d48c53a0f155bf8381355d2862692b09db
atusy <30277794+atusy@users.noreply.github.com>
2023-01-29 23:35:55 +0900

    ✨ show buffer number on <C-G>

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -157,6 +157,7 @@ end, { nargs = 1, range = true, bang = true })
 vim.g.mapleader = ' '
 set_keymap({ 'n', 'v' }, 's', '<Nop>') -- be prefix for sandwich and fuzzy finders
 set_keymap('n', '<C-G>', '<C-G><Plug>(C-G)', { noremap = true })
+set_keymap('n', '<C-G>', '2<C-G><Plug>(C-G)', { noremap = true })
 set_keymap('n', '<Plug>(C-G)<C-G>', '<Cmd>let @+ = fnamemodify(expand("%"), ":~:.")<CR>')
 set_keymap('n', '<Plug>(C-G)g', '<Cmd>let @+ = expand("%:p")<CR>')
 set_keymap('c', '<C-A>', '<Home>')
