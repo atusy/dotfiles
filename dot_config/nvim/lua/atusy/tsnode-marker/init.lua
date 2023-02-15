f4b8fd9d3320f835614ec15e0c289105c64c29c0
atusy <30277794+atusy@users.noreply.github.com>
2023-02-16 08:41:07 +0900

    internal change

--- dot_config/nvim/lua/atusy/tsnode-marker/init.lua
+++ dot_config/nvim/lua/atusy/tsnode-marker/init.lua
@@ -1,159 +1,82 @@
----@alias Tsnode userdata
+---@alias Tsnode userdata should actually be tsnode|under|userdata
+
 ---@class Opts_automark
----@field is_target fun(x: userdata): boolean
----@field hl_group string
----@field linewise? boolean
+---@field is_target string[] | fun(buf: number, node: Tsnode): boolean
+---@field hl_group string | fun(buf: number, node: Tsnode): string
 ---@field priority? number
----@class Opts_mark: Opts_automark
----@field namespace number
----@field start_row? number
----@field end_row? number
+---@field indent? "node" | "none" | fun(buf: number, node: Tsnode): number
 
 local M = {}
 
-local function is_root_node(bufnr, node)
-  if node:parent() ~= nil then
-    return false
-  end
-  local start_row, start_col, end_row, end_col = node:range()
-  if start_row == 0 and start_col == 0 and end_col == 0 then
-    return end_row == vim.api.nvim_buf_line_count(bufnr)
-  end
-  return false
-end
-
-local function list_parent_nodes(node)
-  local list = {}
-  local parent = node
-  while parent ~= nil do
-    table.insert(list, parent)
-    parent = parent:parent()
-  end
-  return list
-end
-
-local function get_first_node_in_range(bufnr, start_row, end_row)
-  local node
-  for row = start_row, end_row do
-    node = vim.treesitter.get_node_at_pos(bufnr, row, 0, {})
-    if node and not is_root_node(bufnr, node) then
-      return node
-    end
-  end
-end
-
----@param buf number
----@param range number[]
----@param opts Opts_mark
-local function set_extmark(buf, range, opts)
-  if opts.linewise ~= false then
-    range = { range[1], 0, range[3] + (range[4] == 0 and 0 or 1), 0 }
-  end
-
-  vim.api.nvim_buf_set_extmark(buf, opts.namespace, range[1], range[2], {
-    end_row = range[3],
-    end_col = range[4],
-    hl_eol = true,
-    priority = opts.priority or 1,
-    hl_group = opts.hl_group,
-  })
-end
-
----@param buf number
----@param node Tsnode
----@param opts Opts_mark
-local function mark_children(buf, node, opts)
-  for k in node:iter_children() do
-    if opts.is_target(k) then
-      set_extmark(buf, { k:range() }, opts)
-    else
-      local sr, _, er, _ = k:range()
-      if (opts.start_row <= sr and sr <= opts.end_row) or (opts.start_row <= er and er <= opts.end_row) then
-        mark_children(buf, k, opts)
-      end
-    end
-  end
-end
-
----@param buf number
----@param node Tsnode
----@param opts Opts_mark
-local function mark_next_sibling(buf, node, opts)
-  local n = node
-  while true do
-    n = n:next_sibling()
-    if n == nil then
-      return
-    end
-    local range = { n:range() }
-    if range[1] <= opts.end_row then
-      if opts.is_target(n) then
-        set_extmark(buf, range, opts)
-      else
-        mark_children(buf, n, opts)
-      end
-    end
-  end
-end
-
----@param buf number
----@param opts Opts_mark
-function M.mark(buf, opts)
-  if not opts.start_row or not opts.end_row then
-    opts = vim.deepcopy(opts)
-    opts.start_row = opts.start_row or 0
-    opts.end_row = opts.end_row or (vim.fn.getpos("$") - 1)
-  end
-  local first_node = get_first_node_in_range(buf, opts.start_row, opts.end_row)
-
-  if first_node == nil then
-    return
-  end
-
-  local ancestors = list_parent_nodes(first_node)
-  for i = #ancestors, 1, -1 do
-    local n = ancestors[i]
-    if opts.is_target(n) then
-      set_extmark(buf, { n:range() }, opts)
-    elseif i == 1 then
-      mark_children(buf, n, opts)
-    end
-    mark_next_sibling(buf, n, opts)
-  end
-end
-
+local current_ns_key = false
 local NAMESPACES = {
-  [false] = vim.api.nvim_create_namespace("atusy-extmark-1"),
-  [true] = vim.api.nvim_create_namespace("atusy-extmark-2"),
+  [false] = vim.api.nvim_create_namespace("tsnode-marker-automark-ns-1"),
+  [true] = vim.api.nvim_create_namespace("tsnode-marker-automark-ns-2"),
 }
-local _current_ns_key = false
 
 ---@param buf number
----@param ns_key boolean
+---@param ns number
 ---@param start_row number
 ---@param end_row number
 ---@param opts Opts_automark
-local function update_namespaces(buf, ns_key, start_row, end_row, opts)
-  M.mark(buf, {
+local function mark(buf, ns, start_row, end_row, opts)
+  require("atusy.tsnode-marker.mark").mark_nodes_in_range(buf, {
     is_target = opts.is_target,
     hl_group = opts.hl_group,
-    linewise = opts.linewise,
-    namespace = NAMESPACES[ns_key],
+    namespace = ns,
     start_row = start_row,
     end_row = end_row,
   })
-  vim.api.nvim_buf_clear_namespace(buf, NAMESPACES[not ns_key], 0, -1)
-  return ns_key
+end
+
+---@param buf number
+---@param namespace number
+local function unmark(buf, namespace)
+  vim.api.nvim_buf_clear_namespace(buf, namespace, 0, -1)
+end
+
+---@param buf number
+---@param start_row number
+---@param end_row number
+---@param opts Opts_automark
+local function refresh(buf, start_row, end_row, opts)
+  local prev = current_ns_key
+  current_ns_key = not current_ns_key
+  mark(buf, NAMESPACES[current_ns_key], start_row, end_row, opts)
+  unmark(buf, NAMESPACES[prev])
+end
+
+---@param buf number
+local function name_automark(buf)
+  return "tsnode-marker-automark-augroup-" .. tostring(buf)
+end
+
+local function clear_namespaces(buf)
+  for _, ns in pairs(NAMESPACES) do
+    vim.api.nvim_buf_clear_namespace(buf, ns, 0, -1)
+  end
+end
+
+---@param buf number?
+function M.unset_automark(buf)
+  local buflist = buf == nil and vim.api.nvim_list_bufs() or { buf }
+  for _, b in pairs(buflist) do
+    local ok, _ = pcall(vim.api.nvim_del_augroup_by_name, name_automark(b))
+    if ok then
+      clear_namespaces(b)
+    end
+  end
 end
 
 ---@param buf number
 ---@param opts Opts_automark
-function M.automark(buf, opts)
+function M.set_automark(buf, opts)
   local first_row = vim.fn.getpos("w0")[2] - 1
   local last_row = vim.fn.getpos("w$")[2] - 1
-  update_namespaces(buf, _current_ns_key, first_row, last_row, opts)
+  clear_namespaces(buf)
+  mark(buf, NAMESPACES[current_ns_key], first_row, last_row, opts)
 
-  local augroup = vim.api.nvim_create_augroup("tsnode-marker-" .. tostring(buf), {})
+  local augroup = vim.api.nvim_create_augroup(name_automark(buf), {})
 
   vim.api.nvim_create_autocmd({
     "TextChanged",
@@ -167,7 +90,7 @@ function M.automark(buf, opts)
       vim.schedule(function()
         first_row = vim.fn.getpos("w0")[2] - 1
         last_row = vim.fn.getpos("w$")[2] - 1
-        _current_ns_key = update_namespaces(buf, not _current_ns_key, first_row, last_row, opts)
+        refresh(buf, first_row, last_row, opts)
       end)
     end,
   })
@@ -182,18 +105,18 @@ function M.automark(buf, opts)
 
       -- on pagewise scroll
       if (first_row > prev_last) or (last_row < prev_first) then
-        _current_ns_key = update_namespaces(buf, not _current_ns_key, first_row, last_row, opts)
+        refresh(buf, first_row, last_row, opts)
         return
       end
 
       -- on linewise scroll up or resize
       if first_row < prev_first then
-        update_namespaces(buf, _current_ns_key, first_row, prev_first - 1, opts)
+        mark(buf, NAMESPACES[current_ns_key], first_row, prev_first - 1, opts)
       end
 
       -- on linewise scroll down or resize
       if last_row > prev_last then
-        update_namespaces(buf, _current_ns_key, prev_last + 1, last_row, opts)
+        mark(buf, NAMESPACES[current_ns_key], prev_last + 1, last_row, opts)
       end
     end,
   })
