34755135aea6be3aa4326e360682603560efceaf
atusy <30277794+atusy@users.noreply.github.com>
2023-02-26 22:42:27 +0900

    refactor: telescope

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,6 @@
 local utils = require("atusy.utils")
 local set_keymap, set_palette = utils.set_keymap, utils.set_palette
 
-local function telescope(key)
-  return "<Cmd>Telescope " .. key .. "<CR>"
-end
-
 local function setup_memo()
   local memo = "~/Documents/memo"
   set_palette(
@@ -20,7 +16,58 @@ local function setup_memo()
   end)
 end
 
-local function setup(_)
+local function telescope_init()
+  local leader = "s"
+  set_keymap("n", leader .. "<CR>", "<Cmd>Telescope builtin<CR>")
+  set_keymap("n", leader .. "<Tab>", "<Cmd>Telescope jumplist<CR>")
+  -- sa is occupied by mini.surround
+  set_keymap("n", leader .. "b", "<Cmd>Telescope buffers<CR>")
+  set_keymap("n", leader .. "c", "<Cmd>Telescope commands<CR>")
+  set_keymap("n", leader .. "b", "<Cmd>Telescope find_files<CR>")
+  -- sd is occupied by mini.surround
+  set_keymap("n", leader .. "g", "<Cmd>Telescope live_grep<CR>")
+  set_keymap("n", leader .. "h", function()
+    pcall(require("atusy.lazy").load_all)
+    require("telescope.builtin").help_tags()
+  end, { desc = "Telescope lazy help_tags" })
+  set_keymap("n", leader .. "o", function()
+    require("plugins.telescope.picker").outline()
+  end, { desc = "Telescope custom outline" })
+  -- sp is occupied by emoji-prefix
+  -- sr is occupied by mini.surround
+  set_keymap("n", leader .. "s", function()
+    require("plugins.telescope.picker").keymaps()
+  end, { desc = "Telescope normal favorite keymaps" })
+  set_keymap("n", leader .. "m", "<Cmd>Telescope keymaps<CR>")
+  set_keymap("n", leader .. "q", "<Cmd>Telescope quickfixhistory<CR>")
+  set_keymap("n", leader .. [[']], "<Cmd>Telescope marks<CR>")
+  set_keymap("n", leader .. [["]], "<Cmd>Telescope registers<CR>")
+  set_keymap("n", leader .. ".", "<Cmd>Telescope resume<CR>")
+  set_keymap("n", leader .. "/", "<Cmd>Telescope current_buffer_fuzzy_find<CR>")
+  set_keymap("n", leader .. "?", "<Cmd>Telescope man_pages<CR>")
+  set_keymap("n", "q;", "<Cmd>Telescope command_history<CR>")
+  -- q: is occupied by cmdbuf.nvim
+  set_keymap("n", "q/", "<Cmd>Telescope search_history<CR>")
+  set_keymap("n", "<Plug>(C-G)<C-S>", "<Cmd>Telescope git_status<CR>")
+
+  setup_memo()
+  vim.api.nvim_create_autocmd("FileType", {
+    group = utils.augroup,
+    pattern = "gitcommit",
+    callback = function(args)
+      local p = require("plugins.telescope.picker.git-prefix")
+      set_keymap("n", leader .. "p", p.prefix_emoji, { buffer = args.buf })
+      local line = vim.api.nvim_buf_get_lines(args.buf, 0, 1, false)
+      if vim.fn.match(line, "^" .. p.regex_emoji) == -1 then
+        vim.schedule(function()
+          p.prefix_emoji(args.buf)
+        end)
+      end
+    end,
+  })
+end
+
+local function telescope_config(_)
   require("atusy.mappings-extra")
   local Telescope = require("telescope")
   Telescope.setup({
@@ -57,80 +104,8 @@ return {
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
       -- 'stevearc/aerial.nvim', -- can be implicit
     },
-    init = function()
-      setup_memo()
-      local leader = "s"
-      for _, v in pairs({
-        { "n", leader .. "<CR>", "builtin" },
-        -- sa is occupied by mini.surround
-        { "n", leader .. "b", "buffers" },
-        { "n", leader .. "c", "commands" },
-        -- sd is occupied by mini.surround
-        { "n", leader .. "f", "find_files" },
-        { "n", leader .. "g", "live_grep" },
-        {
-          "n",
-          leader .. "h",
-          "help_tags",
-          function()
-            pcall(require("atusy.lazy").load_all)
-            require("telescope.builtin").help_tags()
-          end,
-        },
-        { "n", leader .. "<Tab>", "jumplist" },
-        {
-          "n",
-          leader .. "o",
-          "outline",
-          function()
-            require("plugins.telescope.picker").outline()
-          end,
-        },
-        -- sp is occupied by emoji-prefix
-        -- sr is occupied by mini.surround
-        {
-          "n",
-          leader .. "s",
-          "keymaps normal favorites",
-          function()
-            require("plugins.telescope.picker").keymaps()
-          end,
-        },
-        { "n", leader .. "m", "keymaps" },
-        { "n", leader .. "q", "quickfixhistory" },
-        { "n", leader .. [[']], "marks" },
-        { "n", leader .. [["]], "registers" },
-        { "n", leader .. ".", "resume" },
-        { "n", leader .. "/", "current_buffer_fuzzy_find" },
-        { "n", leader .. "?", "man_pages" },
-        { "n", "q;", "command_history" },
-        -- { 'n', 'q:', 'command_history' }, -- prefer cmdbuf.nvim
-        { "n", "q/", "search_history" },
-        { "n", "<Plug>(C-G)<C-S>", "git_status" },
-        -- { 'n', '<Plug>(C-G)<C-M>', 'keymaps' },
-        -- { 'n', '<Plug>(C-G)<CR>', 'keymaps' },
-        -- { { 'i', 'x' }, '<C-G><C-M>', 'keymaps', telescope_keymaps },
-        -- { { 'i', 'x' }, '<C-G><CR>', 'keymaps', telescope_keymaps },
-      }) do
-        set_keymap(v[1], v[2], v[4] or telescope(v[3]), { desc = "telescope " .. v[3] })
-      end
-
-      vim.api.nvim_create_autocmd("FileType", {
-        group = utils.augroup,
-        pattern = "gitcommit",
-        callback = function(args)
-          local p = require("plugins.telescope.picker.git-prefix")
-          set_keymap("n", leader .. "p", p.prefix_emoji, { buffer = args.buf })
-          local line = vim.api.nvim_buf_get_lines(args.buf, 0, 1, false)
-          if vim.fn.match(line, "^" .. p.regex_emoji) == -1 then
-            vim.schedule(function()
-              p.prefix_emoji(args.buf)
-            end)
-          end
-        end,
-      })
-    end,
-    config = setup,
+    init = telescope_init,
+    config = telescope_config,
   },
   {
     "stevearc/aerial.nvim",
