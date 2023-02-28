1cca968b538cab15b63a11e66a9ba99a358f0d51
atusy <30277794+atusy@users.noreply.github.com>
2023-03-01 08:52:13 +0900

    format

--- dot_config/nvim/lua/atusy/utils.lua
+++ dot_config/nvim/lua/atusy/utils.lua
@@ -1,18 +1,22 @@
 local M = {}
 
-M.augroup = 'atusy-init-lua'
-M.datapath = vim.fn.stdpath('data')
+M.augroup = "atusy-init-lua"
+M.datapath = vim.fn.stdpath("data")
 
 function M.require(name)
-  pcall(function() require('plenary.reload').reload_module(name) end)
+  pcall(function()
+    require("plenary.reload").reload_module(name)
+  end)
   return require(name)
 end
 
 function M.safely(f)
-  return function(...) pcall(f, ...) end
+  return function(...)
+    pcall(f, ...)
+  end
 end
 
-M.star = '☆'
+M.star = "☆"
 
 local set_keymap = vim.keymap.set
 function M.set_keymap(mode, lhs, rhs, opt1, opt2)
@@ -36,14 +40,15 @@ end
 -- set keymap as an item of command palette
 -- invoke the itme with Telescope keymaps
 function M.set_palette(mode, lhs, rhs, opts)
-  local space = ' ' -- U+00A0
+  local space = " " -- U+00A0
   vim.keymap.set(
     mode,
     M.star
-    .. lhs:gsub(' ', space) -- replace with U+00A0 to avoid showing <Space>
-    .. space .. space, -- append two spaces to avoid potential waiting
+      .. lhs:gsub(" ", space) -- replace with U+00A0 to avoid showing <Space>
+      .. space
+      .. space, -- append two spaces to avoid potential waiting
     rhs or lhs,
-    vim.tbl_deep_extend('keep', opts or {}, { desc = '' })
+    vim.tbl_deep_extend("keep", opts or {}, { desc = "" })
   )
 end
 
@@ -51,10 +56,7 @@ local ready = false
 function M.setup(force)
   if ready and not force then
     -- avoid accidental reset of augroup
-    vim.notify(
-      'utils.setup has been done before. Force re-run with utils.setup(true).',
-      vim.log.levels.ERROR
-    )
+    vim.notify("utils.setup has been done before. Force re-run with utils.setup(true).", vim.log.levels.ERROR)
     return
   end
   vim.api.nvim_create_augroup(M.augroup, {})
