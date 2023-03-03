182491c71c4d74af809f57ca35f0483c4cc000f6
atusy <30277794+atusy@users.noreply.github.com>
2023-03-03 23:31:23 +0900

    refactor: order of plugins

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -10,58 +10,6 @@ vim.api.nvim_create_autocmd("User", {
 })
 
 local deps = {
-  {
-    "atusy/tsnode-marker.nvim",
-    lazy = true,
-    init = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        group = utils.augroup,
-        pattern = { "lua", "python", "go" },
-        callback = function(ctx)
-          local function is_def(n)
-            return vim.tbl_contains({
-              "func_literal",
-              "function_declaration",
-              "function_definition",
-              "method_declaration",
-              "method_definition",
-            }, n:type())
-          end
-          require("tsnode-marker").set_automark(ctx.buf, {
-            target = function(_, node)
-              if not is_def(node) then
-                return false
-              end
-              local parent = node:parent()
-              while parent do
-                if is_def(parent) then
-                  return true
-                end
-                parent = parent:parent()
-              end
-              return false
-            end,
-            hl_group = "@illuminate",
-          })
-        end,
-      })
-      vim.api.nvim_create_autocmd("FileType", {
-        group = utils.augroup,
-        pattern = "markdown",
-        callback = function(ctx)
-          -- blank target requires capture group @tsodemarker
-          -- dot_config/nvim/after/queries/markdown/highlights.scm
-          require("tsnode-marker").set_automark(ctx.buf, {
-            -- target = { "code_fence_content" },
-            -- hl_group = "@illuminate",
-          })
-        end,
-      })
-    end,
-    config = function()
-      vim.api.nvim_set_hl(0, "@tsnodemarker", { link = "@illuminate" })
-    end,
-  },
   -- basic dependencies
   "tpope/vim-repeat",
   "vim-denops/denops.vim",
@@ -714,6 +662,58 @@ local deps = {
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = function()
       require("nvim-treesitter.configs").setup({ endwise = { enable = true } })
+    end,
+  },
+  {
+    "atusy/tsnode-marker.nvim",
+    lazy = true,
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        group = utils.augroup,
+        pattern = { "lua", "python", "go" },
+        callback = function(ctx)
+          local function is_def(n)
+            return vim.tbl_contains({
+              "func_literal",
+              "function_declaration",
+              "function_definition",
+              "method_declaration",
+              "method_definition",
+            }, n:type())
+          end
+          require("tsnode-marker").set_automark(ctx.buf, {
+            target = function(_, node)
+              if not is_def(node) then
+                return false
+              end
+              local parent = node:parent()
+              while parent do
+                if is_def(parent) then
+                  return true
+                end
+                parent = parent:parent()
+              end
+              return false
+            end,
+            hl_group = "@illuminate",
+          })
+        end,
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+        group = utils.augroup,
+        pattern = "markdown",
+        callback = function(ctx)
+          -- blank target requires capture group @tsodemarker
+          -- dot_config/nvim/after/queries/markdown/highlights.scm
+          require("tsnode-marker").set_automark(ctx.buf, {
+            -- target = { "code_fence_content" },
+            -- hl_group = "@illuminate",
+          })
+        end,
+      })
+    end,
+    config = function()
+      vim.api.nvim_set_hl(0, "@tsnodemarker", { link = "@illuminate" })
     end,
   },
 
