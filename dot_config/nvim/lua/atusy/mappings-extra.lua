b0142da42d9400d0855470cbb90846104c87671f
atusy <30277794+atusy@users.noreply.github.com>
2023-05-17 13:19:43 +0900

    feat!(nvim): change names of extra mappings

--- dot_config/nvim/lua/atusy/mappings-extra.lua
+++ dot_config/nvim/lua/atusy/mappings-extra.lua
@@ -1,16 +1,16 @@
 local set_palette = require("atusy.utils").set_palette
 
 --[[ window ]]
-set_palette("n", "window: horizontally equalize", "<Cmd>hroizontal wincmd =<CR>")
-set_palette("n", "window: vertically equalize", "<Cmd>vertical wincmd =<CR>")
+set_palette("n", "window: equalize horizontally", "<Cmd>hroizontal wincmd =<CR>")
+set_palette("n", "window: equalize vertically", "<Cmd>vertical wincmd =<CR>")
 
 --[[ clipboard ]]
-set_palette("n", "clipboard cwd", "<Cmd>let @+=getcwd()<CR>")
-set_palette("n", "clipboard abs path of of %", '<Cmd>let @+=expand("%:p")<CR>')
-set_palette("n", "clipboard abs path of dirname of %", '<Cmd>let @+=expand("%:p:h")<CR>')
+set_palette("n", "clipboard: copy cwd", "<Cmd>let @+=getcwd()<CR>")
+set_palette("n", "clipboard: copy abs path of of %", '<Cmd>let @+=expand("%:p")<CR>')
+set_palette("n", "clipboard: copy abs path of dirname of %", '<Cmd>let @+=expand("%:p:h")<CR>')
 
 --[[ treesitter ]]
-set_palette("n", "treesitter inspect tree", function()
+set_palette("n", "treesitter: inspect tree", function()
   (vim.treesitter.inspect_tree or vim.treesitter.show_tree)()
 end)
 set_palette(
