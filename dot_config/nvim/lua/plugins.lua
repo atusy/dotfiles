361a29be2e94a619cc28ce7cd59a79905298d4a2
atusy <30277794+atusy@users.noreply.github.com>
2023-05-25 00:44:20 +0900

    feat(nvim): use treesitter features of matchup

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -279,6 +279,16 @@ local deps = {
     event = "BufReadPost",
     config = function()
       vim.g.matchup_matchparen_offscreen = { method = "status_manual" }
+      require("nvim-treesitter.configs").setup({
+        matchup = {
+          enable = true, -- mandatory, false will disable the whole extension
+          -- disable = { "c", "ruby" }, -- optional, list of language that will be disabled
+          -- [options]
+          disable_virtual_text = true,
+          include_match_words = true,
+          enable_quotes = true,
+        },
+      })
     end,
   },
   {
