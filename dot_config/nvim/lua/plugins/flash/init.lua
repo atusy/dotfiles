89327f722bcfec4a83f6da1bbdf7b03cba3257e8
atusy <30277794+atusy@users.noreply.github.com>
2023-07-19 23:18:56 +0900

    feat(nvim): better migemo integrations to incremental flash.nvim

--- dot_config/nvim/lua/plugins/flash/init.lua
+++ dot_config/nvim/lua/plugins/flash/init.lua
@@ -1,10 +1,150 @@
+local function kensaku_query(pat)
+  local str = pat
+  local query = ""
+  for _ = 1, #str do
+    local left, right = string.find(str, " +", 0, false)
+    if left == nil then
+      return query .. vim.fn["kensaku#query"](str)
+    end
+    if left > 1 then
+      query = query .. vim.fn["kensaku#query"](string.sub(str, 1, left - 1))
+    end
+    query = query .. string.rep([[\(\s\|　\)]], right - left + 1)
+    str = string.sub(str, right + 1)
+  end
+  return query
+end
+
+local function search_lines(lines, pat, col1, col2, row)
+  row = row or 0
+  col1 = col1 or 0
+  col2 = col2 or 0
+  local match_str = pat
+  if type(pat) == "string" then
+    local re = vim.regex([[\c]] .. kensaku_query(pat))
+    match_str = function(...)
+      return re:match_str(...)
+    end
+  end
+
+  local n = #lines
+  local matches = {}
+  for i, line in pairs(lines) do
+    local s = line
+    local col = 0
+    if i == n and col2 > 0 then
+      s = s:sub(0, col2 - 1)
+    end
+    if i == 1 then
+      col = col1
+      s = s:sub(col1 + 1)
+    end
+    while s ~= "" do
+      local first, last = match_str(s)
+      if not first then
+        break
+      end
+      table.insert(matches, {
+        pos = { row + i, first + col },
+        end_pos = { row + i, last + col - 1 },
+      })
+      s = s:sub(first + 1)
+      local idx = vim.fn.byteidx(s, 1)
+      s = s:sub(idx + 1)
+      col = col + first + idx
+    end
+  end
+  return matches
+end
+
+local function search(buf, pat, start, _end, strict_indexing)
+  start = start or { 0, 0 }
+  _end = _end or { -1, 0 }
+  local end_ = _end[1] + ((_end[1] > 0 and _end[2] > 0) and 1 or 0)
+  local lines = vim.api.nvim_buf_get_lines(buf, start[1], end_, strict_indexing or true)
+  return search_lines(lines, pat, start[2], _end[2], start[1]), lines
+end
+
+local function key(m)
+  return "w" .. m.win .. "r" .. m.pos[1] .. "c" .. m.pos[2]
+end
+
+local function sort(tbl, win)
+  local cursor = vim.api.nvim_win_get_cursor(win)
+  table.sort(tbl, function(a, b)
+    local ap, bp = a.pos, b.pos
+    if ap[1] == bp[1] then
+      return ap[2] < bp[2]
+    end
+    local dya = cursor[1] - ap[1]
+    local dyb = cursor[1] - bp[1]
+    return dya * dya < dyb * dyb
+  end)
+  return tbl
+end
+
+local function matcher(cache)
+  local curwin = vim.api.nvim_get_current_win()
+  local labels
+  if cache then
+    cache.labels = {}
+    labels = {}
+  end
+  return function(win, state)
+    local wininfo = vim.fn.getwininfo(win)
+    if win ~= curwin or state.pattern.pattern == "" or not wininfo or #wininfo == 0 then
+      return {}
+    end
+    local buf, top, bot = wininfo[1].bufnr, wininfo[1].topline - 1, wininfo[1].botline
+    local matches, lines = search(buf, state.pattern.pattern, { top, 0 }, { bot, 0 })
+    local used = {}
+    for _, m in pairs(matches) do
+      m.win = win
+      if labels then
+        m.label = labels[key(m)]
+        if m.label then
+          used[m.label] = true
+        end
+      end
+    end
+
+    if win == curwin then
+      matches = sort(matches, win)
+    end
+
+    -- 大文字以外はmigemoと干渉しないように扱う
+    local i = 0
+    local s = table.concat(lines, "\n")
+    for lab in string.gmatch(state.opts.labels, ".") do
+      if not matches[i + 1] then
+        return matches
+      end
+      if
+        not used[lab]
+        and (
+          string.match(lab, "[ABCDEFGHIJKLMNOPQRSTUVWXYZ]")
+          or not vim.regex([[\c]] .. kensaku_query(state.pattern.pattern .. lab)):match_str(s)
+        )
+      then
+        i = i + 1
+        if not matches[i].label then
+          matches[i].label = lab
+          if labels then
+            labels[key(matches[i])] = lab
+          end
+        end
+      end
+    end
+    return matches
+  end
+end
+
 return {
   {
     "folke/flash.nvim",
     lazy = true,
     dir = "~/ghq/github.com/folke/flash.nvim",
     init = function(p)
-      local leader = ""
       local Config = require("flash.config")
       local motions = {
         f = { label = { after = false, before = { 0, 0 } } },
@@ -22,7 +162,7 @@ return {
       }
 
       for _, motion in ipairs({ "f", "t", "F", "T" }) do
-        vim.keymap.set({ "n", "x", "o" }, leader .. motion, function()
+        vim.keymap.set({ "n", "x", "o" }, motion, function()
           require("flash").jump(Config.get({
             mode = "char",
             jump = {
@@ -51,32 +191,17 @@ return {
           }, motions[motion]))
         end)
       end
-      vim.keymap.set({ "n", "x", "o" }, leader .. ";", function()
-        local curwin = vim.api.nvim_get_current_win()
+      vim.keymap.set({ "n", "x", "o" }, ";", function()
+        local cache = {}
+
         require("flash").jump({
-          labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-          label = { before = { 0, 0 }, after = false },
-          matcher = function(win, state)
-            if win ~= curwin then
-              return {}
-            end
-            local matches = require("leap-search.engine").search(state.pattern.pattern, {
-              engines = {
-                { name = "string.find", plain = true, ignorecase = true },
-                { name = "kensaku.query" },
-              },
-            }, { target_windows = { win } })
-            local ret = {}
-            for _, m in pairs(matches) do
-              table.insert(ret, {
-                win = m.wininfo.winid,
-                pos = { m.pos[1], m.pos[2] - 1 },
-                end_pos = { m.pos[3], m.pos[4] - 2 },
-              })
-            end
-            return ret
-          end,
+          labels = [[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()[]`'=-{}~"+_ ]],
+          label = { before = true, after = false },
+          matcher = matcher(cache),
+          labeler = function() end,
         })
+
+        cache = {}
       end)
     end,
     config = function()
