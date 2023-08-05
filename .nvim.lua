864ecf154ac0d308ec2b1f1e7110c3adc2bd32ef
atusy <30277794+atusy@users.noreply.github.com>
2023-08-05 20:52:51 +0900

    refactor(nvim): use vim.system for chezmoi apply on VimLeave

--- .nvim.lua
+++ .nvim.lua
@@ -1,12 +1,9 @@
-local chezmoi = vim.api.nvim_exec('pwd', true)
-local group = vim.api.nvim_create_augroup('nvimrc-chezmoi', {})
-vim.api.nvim_create_autocmd('VimLeave', {
+local group = vim.api.nvim_create_augroup("nvimrc-chezmoi", {})
+vim.api.nvim_create_autocmd("VimLeave", {
   callback = function(args)
-    if vim.startswith(args.file, '.git/') then return end
-    vim.cmd('!chezmoi apply')
+    vim.system({ "chezmoi", "apply" }):wait()
   end,
   group = group,
-  pattern = chezmoi .. '/*',
 })
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = '*.lua',
