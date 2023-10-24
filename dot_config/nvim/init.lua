9faf7cc42cc5a9446f2995ce6926e11a7b8edcba
atusy <30277794+atusy@users.noreply.github.com>
2023-10-24 10:08:05 +0900

    feat!(nvim): remove fold column from status column

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -63,7 +63,7 @@ vim.opt.updatetime = 250
 -- statuscolumn
 vim.opt.signcolumn = "yes"
 vim.opt.foldcolumn = "auto"
-vim.opt.statuscolumn = "%=%{&rnu ? v:relnum ? v:relnum : v:lnum : &nu ? v:lnum : ''}%s%C"
+vim.opt.statuscolumn = "%=%{&rnu ? v:relnum ? v:relnum : v:lnum : &nu ? v:lnum : ''}%s"
 
 -- window
 vim.opt.splitright = true
