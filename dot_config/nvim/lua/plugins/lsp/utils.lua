966076320e50f76f6432fbe7c35e0a8b7e6e69d9
atusy <30277794+atusy@users.noreply.github.com>
2023-10-29 00:22:48 +0900

    fix(nvim): typo

--- dot_config/nvim/lua/plugins/lsp/utils.lua
+++ dot_config/nvim/lua/plugins/lsp/utils.lua
@@ -4,7 +4,7 @@ function M.attach_lsp(bufnr, filetype)
   bufnr = bufnr or vim.api.nvim_get_current_buf()
   filetype = filetype or vim.api.nvim_get_option_value("filetype", { buf = bufnr })
   local clients = {}
-  for _, cl in ipairs(vim.lsp.ge_clients()) do
+  for _, cl in ipairs(vim.lsp.get_clients()) do
     if not cl.attached_buffers[bufnr] then
       if cl.config and cl.config.filetypes then
         for _, ft in ipairs(cl.config.filetypes) do
