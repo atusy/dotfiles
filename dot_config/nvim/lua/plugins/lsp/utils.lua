17d216a9dff4cfe6f76ae6d0a4a9d7561cc112a0
atusy <30277794+atusy@users.noreply.github.com>
2023-10-29 00:17:50 +0900

    refacotr(nvim): remove deprecated function

--- dot_config/nvim/lua/plugins/lsp/utils.lua
+++ dot_config/nvim/lua/plugins/lsp/utils.lua
@@ -4,7 +4,7 @@ function M.attach_lsp(bufnr, filetype)
   bufnr = bufnr or vim.api.nvim_get_current_buf()
   filetype = filetype or vim.api.nvim_get_option_value("filetype", { buf = bufnr })
   local clients = {}
-  for _, cl in ipairs(vim.lsp.get_active_clients()) do
+  for _, cl in ipairs(vim.lsp.ge_clients()) do
     if not cl.attached_buffers[bufnr] then
       if cl.config and cl.config.filetypes then
         for _, ft in ipairs(cl.config.filetypes) do
@@ -21,7 +21,7 @@ end
 
 function M.has_lsp_client(bufnr)
   bufnr = bufnr or vim.api.nvim_get_current_buf()
-  for _, _ in pairs(vim.lsp.get_active_clients({ bufnr = bufnr })) do
+  for _, _ in pairs(vim.lsp.get_clients({ bufnr = bufnr })) do
     return true
   end
   return false
