3f8053813138259036f86045c737c87de760f5ea
atusy <30277794+atusy@users.noreply.github.com>
2023-11-14 13:02:19 +0900

    feat(nvim): generalize uri injection query

--- dot_config/nvim/after/queries/lua/injections.scm
+++ dot_config/nvim/after/queries/lua/injections.scm
@@ -2,6 +2,6 @@
 (
   string
   (string_content) @injection.content
-  (#vim-match? @injection.content "^https://github\\.com/[^/]+/[^/]+$")
+  (#vim-match? @injection.content "^[a-zA-Z][a-zA-Z0-9]*:\/\/\\S\+$")
   (#set! injection.language "uri")
 )
