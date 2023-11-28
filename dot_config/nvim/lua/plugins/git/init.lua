354f7c7c97cbaf2166cc7204972201fdcf3411be
atusy <30277794+atusy@users.noreply.github.com>
2023-11-28 10:04:27 +0900

    feat(nvim): update git settings

--- dot_config/nvim/lua/plugins/git/init.lua
+++ dot_config/nvim/lua/plugins/git/init.lua
@@ -1,75 +1,46 @@
-local api = vim.api
-
 local utils = require("atusy.utils")
-local set_keymap = vim.keymap.set
-local add_palette = require("atusy.keymap.palette").add_item
+local add_item = require("atusy.keymap.palette").add_item
 
 -- gitsigns settings
 local function setup_gitsigns()
-  local gs = require("gitsigns")
   local has_num = vim.opt.number:get() or vim.opt.relativenumber:get()
-  local function after_save(x, cmd)
-    return "<Plug>(save)" .. (cmd or "<Cmd>") .. "Gitsigns " .. x .. "<CR>"
-  end
 
-  vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", { link = "Comment" })
-  add_palette("n", "toggle current line git blame", "<Cmd>Gitsigns toggle_current_line_blame<CR>")
-
-  gs.setup({
+  require("gitsigns").setup({
     signcolumn = not has_num,
     numhl = has_num,
-    current_line_blame_opts = {
-      delay = 150,
-    },
-    on_attach = function(bufnr)
-      set_keymap(
-        "x",
-        "<Plug>(C-G)<C-A>",
-        after_save("stage_hunk", [[<C-\><C-N>gv:]]),
-        { buffer = bufnr, desc = "git add hunk" }
-      )
-      set_keymap("n", "<Plug>(C-G)<C-A>", after_save("stage_hunk"), { buffer = bufnr, desc = "git add hunk" })
-      set_keymap("n", "<Plug>(C-G)a", after_save("stage_buffer"), { buffer = bufnr, desc = "git add buffer" })
-      set_keymap("n", "<Plug>(C-G)<C-R>", after_save("reset_hunk"), { buffer = bufnr, desc = "git reset hunk" })
-      set_keymap("x", "<Plug>(C-G)<C-R>", after_save("reset_hunk", ":"), { buffer = bufnr, desc = "git reset hunk" })
-      set_keymap("n", "<Plug>(C-G)r", after_save("reset_buffer"), { buffer = bufnr, desc = "git reset buffer" })
-      set_keymap("n", "<Plug>(C-G)<C-H>", after_save("preview_hunk"), { buffer = bufnr, desc = "git preview hunk" })
-
-      set_keymap("n", "<Down>", function()
-        if vim.wo.diff then
-          return "]c"
-        end
-        vim.schedule(function()
-          gs.next_hunk()
-        end)
-        return "<Ignore>"
-      end, { expr = true })
-
-      set_keymap("n", "<Up>", function()
-        if vim.wo.diff then
-          return "[c"
-        end
-        vim.schedule(function()
-          gs.prev_hunk()
-        end)
-        return "<Ignore>"
-      end, { expr = true })
+    current_line_blame_opts = { delay = 150 },
+    on_attach = function(buf)
+      local function save_and(x, cmd)
+        return "<Plug>(save)" .. (cmd or "<Cmd>") .. "Gitsigns " .. x .. "<CR>"
+      end
+      vim.keymap.set({ "n", "x" }, "<Plug>(C-G)<C-A>", save_and("stage_hunk", ":"), { buffer = buf }) -- add hunk
+      vim.keymap.set({ "n", "x" }, "<Plug>(C-G)<C-R>", save_and("reset_hunk", ":"), { buffer = buf }) -- reset hunk
+      vim.keymap.set("n", "<Plug>(C-G)a", save_and("stage_buffer"), { buffer = buf }) -- add buf
+      vim.keymap.set("n", "<Plug>(C-G)r", save_and("reset_buffer"), { buffer = buf }) --reset buf
+      vim.keymap.set("n", "<Plug>(C-G)<C-H>", save_and("preview_hunk"), { buffer = buf }) -- preview hunk
+      vim.keymap.set("n", "<Plug>(C-G)<C-U>", save_and("undo_stage_hunk"), { buffer = buf }) -- undo add hunk
     end,
   })
-  add_palette("n", "gitsigns based on cWORD ref", function()
+
+  -- keymaps
+  vim.keymap.set("n", "<Down>", function()
+    return vim.wo.diff and "]c" or "<Cmd>Gitsigns next_hunk<CR>"
+  end, { expr = true })
+
+  vim.keymap.set("n", "<Up>", function()
+    return vim.wo.diff and "[c" or "<Cmd>Gitsigns prev_hunk<CR>"
+  end, { expr = true })
+
+  -- command palette
+  add_item("n", "gitsigns: based on cWORD ref", function()
     require("gitsigns").change_base(vim.fn.expand("<cWORD>"), true)
   end)
-  add_palette("n", "gitsigns based on HEAD", function()
-    require("gitsigns").reset_base(true)
-  end)
-  add_palette("n", "gitsigns qflist", function()
-    require("gitsigns").setqflist("all")
-  end)
-  add_palette("n", "gitsigns toggle word diff", function()
-    require("gitsigns").toggle_word_diff()
-  end)
-  add_palette("n", "gitsings undo add hunk", function()
-    require("gitsings").undo_stage_hunk()
+  add_item("n", "gitsigns: based on HEAD", [[<Cmd>lua require("gitsigns").reset_base(true)<CR>]])
+  add_item("n", "gitsigns: diff to qflist", [[<Cmd>lua require("gitsigns").setqflist("all")<CR>]])
+  add_item("n", "gitsigns: toggle word diff", [[<Cmd>Gitsigns toggle_word_diff<CR>]])
+  add_item("n", "gitsigns: toggle line blame", function()
+    vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", { link = "Comment" }) -- be lazy
+    require("gitsigns").toggle_current_line_blame()
   end)
 end
 
@@ -88,25 +59,32 @@ local function setup_gin()
       GinBuffer = { processor = processor },
     },
   })
-  api.nvim_create_autocmd("BufReadCmd", {
+  vim.api.nvim_create_autocmd("BufReadCmd", {
+    group = utils.augroup,
+    pattern = { "gin://*", "ginedit://*" },
+    callback = function(ctx)
+      vim.keymap.set("n", "<F5>", "<Cmd>call gin#util#reload()<CR>", { buffer = ctx.buf })
+    end,
+  })
+  vim.api.nvim_create_autocmd("BufReadCmd", {
     group = utils.augroup,
     pattern = "ginedit://*",
     callback = function(ctx)
       -- for GinPatch
-      set_keymap("n", "<Plug>(C-G)<C-A>", "<Plug>(gin-diffget-r)", { buffer = ctx.buf }) -- git add
-      set_keymap("n", "<Plug>(C-G)<C-R>", "<Plug>(gin-diffget-l)", { buffer = ctx.buf }) -- git reset
+      vim.keymap.set("n", "<Plug>(C-G)<C-A>", "<Plug>(gin-diffget-r)", { buffer = ctx.buf }) -- git add
+      vim.keymap.set("n", "<Plug>(C-G)<C-R>", "<Plug>(gin-diffget-l)", { buffer = ctx.buf }) -- git reset
     end,
   })
-  api.nvim_create_autocmd("FileType", {
+  vim.api.nvim_create_autocmd("FileType", {
     group = utils.augroup,
     pattern = "gintonic-graph",
     callback = function()
       vim.opt_local.cursorline = true
     end,
   })
-  api.nvim_create_autocmd("FileType", {
+  vim.api.nvim_create_autocmd("FileType", {
     group = utils.augroup,
-    pattern = "gitcommit",
+    pattern = { "gitcommit", "gitrebase" },
     callback = function()
       vim.g.gin_proxy_apply_without_confirm = 1
       vim.keymap.set("ca", "q!", function()
@@ -122,27 +100,8 @@ local function setup_gin()
     end,
   })
 
-  local function yank_git_link(ginopts, opts)
-    opts = opts or {}
-    local range = ""
-    if ginopts.range == 1 then
-      range = tostring(ginopts.line1)
-    elseif ginopts.range == 2 then
-      range = ginopts.line1 .. "," .. ginopts.line2
-    end
-    vim.cmd(range .. "GinBrowse ++yank=+ -n " .. (opts.permalink and "--permalink " or "") .. ginopts.args)
-  end
-
-  vim.api.nvim_create_user_command("GitPermalink", function(opts)
-    yank_git_link(opts, { permalink = true })
-  end, { range = true })
-
-  vim.api.nvim_create_user_command("GitLink", function(opts)
-    yank_git_link(opts, {})
-  end, { range = true })
-
   -- keymaps
-  set_keymap("n", "<Plug>(C-G)<C-P>", "<Cmd>GinPatch ++opener=tabnew %<CR>")
+  vim.keymap.set("n", "<Plug>(C-G)<C-P>", "<Cmd>GinPatch ++opener=tabnew %<CR>")
   local graph = function(opts)
     opts = opts and (" " .. opts) or ""
     if vim.api.nvim_get_option_value("filetype", { buf = 0 }) ~= "gintonic-graph" then
@@ -150,34 +109,34 @@ local function setup_gin()
     end
     vim.cmd("GintonicGraph" .. opts)
   end
-  set_keymap("n", "<Plug>(C-G)<C-L>", function()
+  vim.keymap.set("n", "<Plug>(C-G)<C-L>", function()
     graph("-- %")
   end, { desc = "git graph current buffer" })
-  set_keymap("n", "<Plug>(C-G)l", function()
+  vim.keymap.set("n", "<Plug>(C-G)l", function()
     graph()
   end, { desc = "git graph" }) -- git log --graph ...
-  set_keymap("n", "<Plug>(C-G)<C-D>", "<Cmd>GinDiff -- %<CR>")
-  set_keymap("n", "<Plug>(C-G)d", "<Cmd>GinDiff<CR>")
-  set_keymap("n", "<Plug>(C-G)<C-Space>", function()
-    require("plugins.git.commit").exec()
-  end, { desc = "git commit" })
-  set_keymap("n", "<Plug>(C-G)<C-X>", "v:GinBrowse<CR>") -- anologous to gx
-  set_keymap("x", "<Plug>(C-G)<C-X>", ":GinBrowse<CR>")
+  vim.keymap.set("n", "<Plug>(C-G)<C-D>", "<Cmd>GinDiff -- %<CR>")
+  vim.keymap.set("n", "<Plug>(C-G)d", "<Cmd>GinDiff<CR>")
+  vim.keymap.set("n", "<Plug>(C-G)<C-Space>", [[<Cmd>lua require("plugins.git.commit").exec()<CR>]]) -- commit
+  vim.keymap.set("n", "<Plug>(C-G)<C-X>", "v:GinBrowse<CR>") -- anologous to gx
+  vim.keymap.set("x", "<Plug>(C-G)<C-X>", ":GinBrowse<CR>")
+  vim.keymap.set("n", "<Plug>(C-G)<C-Y>", [[v:GinBrowse ++yank=+ -n --permalink]]) -- <CR> to confirm
+  vim.keymap.set("x", "<Plug>(C-G)<C-Y>", [[:GinBrowse ++yank=+ -n --permalink]])
 
   -- command palette
-  add_palette("n", "git amend", ":Gin commit --amend ")
-  add_palette("n", "git amend --no-edit", ":Gin ++wait commit --amend --no-edit ")
-  add_palette("n", "git rebase -i", ":Gin rebase --rebase-merge -i ")
-  add_palette(
+  add_item("n", "git amend", ":Gin commit --amend ")
+  add_item("n", "git amend --no-edit", ":Gin ++wait commit --amend --no-edit ")
+  add_item("n", "git rebase -i", ":Gin rebase --rebase-merge -i ")
+  add_item(
     "n",
     "git rebase --onto A B C",
     ":Gin rebase --rebase-merge --onto ",
     { desc = "AにBからCまでの差分を乗せる" }
   )
-  add_palette("n", "git push", ":Gin ++wait push origin HEAD ")
-  add_palette("n", "git push --force", ":Gin ++wait push --force-with-lease --force-if-includes origin HEAD ")
-  add_palette("n", "git diff", ":GinDiff ")
-  add_palette("n", "git diff --ignore-all-space", ":GinDiff --ignore-all-space")
+  add_item("n", "git push", ":Gin ++wait push origin HEAD ")
+  add_item("n", "git push --force", ":Gin ++wait push --force-with-lease --force-if-includes origin HEAD ")
+  add_item("n", "git diff", ":GinDiff ")
+  add_item("n", "git diff --ignore-all-space", ":GinDiff --ignore-all-space ")
 end
 
 -- return
