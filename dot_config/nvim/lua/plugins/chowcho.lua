f4500c40c3e83a76181d7b328d086c9bd0f94634
atusy <30277794+atusy@users.noreply.github.com>
2023-12-07 09:21:02 +0900

    refactor(nvim): chowcho

--- dot_config/nvim/lua/plugins/chowcho.lua
+++ dot_config/nvim/lua/plugins/chowcho.lua
@@ -1,11 +1,5 @@
-local api = vim.api
-local list_wins = api.nvim_tabpage_list_wins
-local set_keymap = vim.keymap.set
-
-local safely = require("atusy.utils").safely
-
 local function is_win_focusable(win)
-  local config = api.nvim_win_get_config(win)
+  local config = vim.api.nvim_win_get_config(win)
   return config.focusable
 end
 
@@ -14,6 +8,8 @@ local function count_win_focusable(wins)
 end
 
 local setup_chowcho = function()
+  local safely = require("atusy.utils").safely
+
   require("chowcho").setup({
     use_exclude_default = false,
     exclude = function(_, _)
@@ -24,11 +20,11 @@ local setup_chowcho = function()
 
   local function chowcho_focus()
     -- Focues window
-    if count_win_focusable(list_wins(0)) > 2 then
-      run(safely(api.nvim_set_current_win), {
+    if count_win_focusable(vim.api.nvim_tabpage_list_wins(0)) > 2 then
+      run(safely(vim.api.nvim_set_current_win), {
         use_exclude_default = false,
         exclude = function(_, win)
-          return api.nvim_win_get_config(win).focusable == false
+          return vim.api.nvim_win_get_config(win).focusable == false
         end,
       })
     else
@@ -36,45 +32,45 @@ local setup_chowcho = function()
     end
   end
 
-  set_keymap({ "", "t" }, "<C-W><C-W>", chowcho_focus)
-  set_keymap({ "", "t" }, "<C-W>w", chowcho_focus)
+  vim.keymap.set({ "", "t" }, "<C-W><C-W>", chowcho_focus)
+  vim.keymap.set({ "", "t" }, "<C-W>w", chowcho_focus)
 
   local function _chowcho_hide()
-    local nwins = #list_wins(0)
-    if nwins == 1 and api.nvim_get_current_tabpage() ~= 1 then
+    local nwins = #vim.api.nvim_tabpage_list_wins(0)
+    if nwins == 1 and vim.api.nvim_get_current_tabpage() ~= 1 then
       vim.cmd("tabclose")
     elseif nwins == 2 then
       vim.cmd("wincmd o")
     elseif nwins > 2 then
-      run(safely(api.nvim_win_hide))
+      run(safely(vim.api.nvim_win_hide))
     end
   end
 
-  -- set_keymap({ '', 't' }, '<C-W>c', _chowcho_hide)
-  set_keymap({ "", "t" }, "<C-W><Space>", _chowcho_hide)
-  set_keymap({ "", "t" }, "<C-W><C-Space>", _chowcho_hide)
+  -- vim.keymap.set({ '', 't' }, '<C-W>c', _chowcho_hide)
+  vim.keymap.set({ "", "t" }, "<C-W><Space>", _chowcho_hide)
+  vim.keymap.set({ "", "t" }, "<C-W><C-Space>", _chowcho_hide)
 
   local function _chowcho_edit()
     -- Edits buffer from the selected in the current
-    if #list_wins(0) < 1 then
+    if #vim.api.nvim_tabpage_list_wins(0) < 1 then
       return
     end
     run(
       safely(function(n)
-        api.nvim_win_set_buf(0, api.nvim_win_get_buf(n))
+        vim.api.nvim_win_set_buf(0, vim.api.nvim_win_get_buf(n))
       end),
       {
         use_exclude_default = false,
         exclude = function(_, win)
-          local winconf = api.nvim_win_get_config(win)
+          local winconf = vim.api.nvim_win_get_config(win)
           return winconf.external or winconf.relative ~= ""
         end,
       }
     )
   end
 
-  set_keymap({ "", "t" }, "<C-W>e", _chowcho_edit)
-  set_keymap({ "", "t" }, "<C-W><C-E>", _chowcho_edit)
+  vim.keymap.set({ "", "t" }, "<C-W>e", _chowcho_edit)
+  vim.keymap.set({ "", "t" }, "<C-W><C-E>", _chowcho_edit)
 
   ---@param layout table returnd by vim.fn.winlayout()
   ---@param cur integer of the current winid
@@ -101,13 +97,13 @@ local setup_chowcho = function()
   local function _chowcho_exchange()
     -- Swaps buffers between windows
     -- also moves between windows to apply styler.nvim theming
-    if #list_wins(0) <= 2 then
+    if #vim.api.nvim_tabpage_list_wins(0) <= 2 then
       vim.cmd("wincmd x")
       return
     end
     run(
       safely(function(n)
-        local cur = api.nvim_get_current_win()
+        local cur = vim.api.nvim_get_current_win()
         if n == cur then
           return
         end
@@ -116,23 +112,23 @@ local setup_chowcho = function()
           vim.cmd(count .. "wincmd x")
           return
         end
-        local bufnr0 = api.nvim_win_get_buf(cur)
-        local bufnrn = api.nvim_win_get_buf(n)
-        api.nvim_win_set_buf(cur, bufnrn)
-        api.nvim_win_set_buf(n, bufnr0)
+        local bufnr0 = vim.api.nvim_win_get_buf(cur)
+        local bufnrn = vim.api.nvim_win_get_buf(n)
+        vim.api.nvim_win_set_buf(cur, bufnrn)
+        vim.api.nvim_win_set_buf(n, bufnr0)
       end),
       {
         use_exclude_default = false,
         exclude = function(_, win)
-          local winconf = api.nvim_win_get_config(win)
+          local winconf = vim.api.nvim_win_get_config(win)
           return winconf.external or winconf.relative ~= ""
         end,
       }
     )
   end
 
-  set_keymap({ "", "t" }, "<C-W><C-X>", _chowcho_exchange)
-  set_keymap({ "", "t" }, "<C-W>x", _chowcho_exchange)
+  vim.keymap.set({ "", "t" }, "<C-W><C-X>", _chowcho_exchange)
+  vim.keymap.set({ "", "t" }, "<C-W>x", _chowcho_exchange)
 end
 
 return {
