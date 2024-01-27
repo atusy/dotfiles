03b3aecc98dba52f253d4f4f56af7e12969e74a6
atusy <30277794+atusy@users.noreply.github.com>
2024-01-28 00:28:26 +0900

    fix(nvim): using denoKv() requires --unstable-kv

--- dot_config/nvim/lua/plugins/denops/init.lua
+++ dot_config/nvim/lua/plugins/denops/init.lua
@@ -5,6 +5,7 @@ return {
 			local bin, cache = require("plugins.denops.utils").get_deno("1.40.2")
 			vim.g["denops#deno"] = bin
 			vim.g["denops#deno_dir"] = cache
+			vim.g["denops#server#deno_args"] = { "-q", "--no-lock", "-A", "--unstable-kv" }
 			require("plugins.denops.utils").cache_plugin(p)
 		end,
 	},
