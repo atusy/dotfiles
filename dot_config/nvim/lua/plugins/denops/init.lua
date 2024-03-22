1d09dd7463bdb4457c5d77ebd41fc1bc41d1e8fc
atusy <30277794+atusy@users.noreply.github.com>
2024-03-22 22:26:31 +0900

    chore(nvim): update denops#deno to 1.41.2

--- dot_config/nvim/lua/plugins/denops/init.lua
+++ dot_config/nvim/lua/plugins/denops/init.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"https://github.com/vim-denops/denops.vim",
 		init = function(p)
-			local bin, cache = require("plugins.denops.utils").get_deno("1.40.2")
+			local bin, cache = require("plugins.denops.utils").get_deno("1.41.3")
 			vim.g["denops#deno"] = bin
 			vim.g["denops#deno_dir"] = cache
 			vim.g["denops#server#deno_args"] = { "-q", "--no-lock", "-A", "--unstable-kv" }
