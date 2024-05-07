c6f77182cc4048a150c6c6327088262823305cea
atusy <30277794+atusy@users.noreply.github.com>
2024-05-07 13:39:39 +0900

    perf(nvim): improve deno cache

--- dot_config/nvim/lua/atusy/bin/deno.lua
+++ dot_config/nvim/lua/atusy/bin/deno.lua
@@ -11,25 +11,29 @@ function M.install(version, opts, on_exit)
 	return obj, vim.fs.joinpath(opts.env.DENO_INSTALL, "bin/deno")
 end
 
-M.cache_pending = {} ---@type table<string, true>
+M.cache_pending = {} ---@type table<string, string[]>
 M.cache_ongoing = 0
 
 ---Deno cache asyncronously
 ---
 ---Queues a ts file to be cached, and parallelly consumes up to 5 queued files.
 ---The consumption order may be inconsitent with the queued order.
-function M.cache(ts, bin, cache)
+---@param tsfiles string[] paths of ts files
+---@param bin string path to deno executable
+---@param cache string path to deno cache
+function M.cache(tsfiles, bin, cache)
+	local key = table.concat(tsfiles, "\n")
 	if M.cache_ongoing > 5 then
-		M.cache_pending[ts] = true
+		M.cache_pending[key] = tsfiles
 		return
 	end
 	M.cache_ongoing = M.cache_ongoing + 1
-	return vim.system({ bin or "deno", "cache", ts }, { env = { DENO_DIR = cache } }, function()
-		M.cache_pending[ts] = nil
+	return vim.system({ bin or "deno", "cache", unpack(tsfiles) }, { env = { DENO_DIR = cache } }, function()
+		M.cache_pending[key] = nil
 		M.cache_ongoing = M.cache_ongoing - 1
-		for pending, _ in pairs(M.cache_pending) do
-			M.cache_pending[pending] = nil
-			M.cache(pending, bin, cache)
+		for nextkey, nextfiles in pairs(M.cache_pending) do
+			M.cache_pending[nextkey] = nil
+			M.cache(nextfiles, bin, cache)
 			return
 		end
 	end)
@@ -42,9 +46,13 @@ function M.cache_dir(dir, bin, cache)
 	local cmd = { "fd", ".", "--type", "f", "-e", "ts" }
 	vim.notify("deno cache " .. dir)
 	return vim.system(cmd, { cwd = dir, text = true }, function(fd)
+		local tsfiles = {}
 		if fd.stdout then
 			for ts in fd.stdout:gmatch("[^\n]+") do
-				M.cache(vim.fs.joinpath(dir, ts), bin, cache)
+				table.insert(tsfiles, vim.fs.joinpath(dir, ts))
+			end
+			if #tsfiles > 0 then
+				M.cache(tsfiles, bin, cache)
 			end
 		end
 	end)
