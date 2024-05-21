3c2c9957890a5df92824c6c08cc41e60c7f950b6
atusy <30277794+atusy@users.noreply.github.com>
2024-05-21 20:38:19 +0900

    refactor(wezterm): format-tab-title

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,18 +1,14 @@
 local wezterm = require("wezterm")
 
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local pane_title = tab.active_pane.title
-	local user_title = tab.active_pane.user_vars.panetitle
+wezterm.on("format-tab-title", function(window, pane)
+	local pane_title = window.active_pane.title
+	local user_title = window.active_pane.user_vars.panetitle
 
 	if user_title ~= nil and #user_title > 0 then
 		pane_title = user_title
 	end
 
-	return {
-		--{Background={Color="black"}},
-		--{Foreground={Color="white"}},
-		{ Text = " " .. pane_title .. " " },
-	}
+	return { { Text = " " .. pane_title .. " " } }
 end)
 
 local default_opacity = 0.80
