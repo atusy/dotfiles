ef1b1e27dd2bdf45d66c8244179be1f4f9e85b28
atusy <30277794+atusy@users.noreply.github.com>
2024-05-22 16:39:24 +0900

    feat(fish): show login info iff on SSH and show suffix in new line

--- dot_config/private_fish/functions/fish_prompt.fish
+++ dot_config/private_fish/functions/fish_prompt.fish
@@ -27,5 +27,8 @@ function fish_prompt --description 'Write out the prompt'
     set -l statusb_color (set_color $bold_flag $fish_color_status)
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-    echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
+    if ! test -z $SSH_CLIENT
+      echo -n -s (prompt_login)' '
+    end
+    echo -n -s -e (set_color $color_cwd) (prompt_pwd -D 3) $normal (fish_vcs_prompt) $normal " "$prompt_status "\n" $suffix " "
 end
