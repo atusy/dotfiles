75c53eeee46832041f1e385795244c716ea71f0c
atusy <30277794+atusy@users.noreply.github.com>
2024-05-25 13:08:54 +0900

    feat(fish): add gh-fzf

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -28,3 +28,22 @@ alias ls='eza'
 alias sudo='sudo '
 alias vim='nvim'
 alias wget='wget -c'
+
+# HACK: fish support for gh-fzf
+function gh
+  set -l ghf $HOME/ghq/github.com/atusy/gh-fzf/bin/ghf 
+
+  # bootstrap
+  if ! test -x $ghf
+    ghq get atusy/gh-fzf
+  end
+
+  # hacks
+  if ! test -t 0
+    cat - | xargs gh $argv
+  else if test -t 1 || set -q _GH_FZF_VIEWER
+    $ghf $argv
+  else
+    _GH_FZF_VIEWER=id $ghf $argv
+  end
+end
