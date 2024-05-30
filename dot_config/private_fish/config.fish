5cbc8eabde7899e7364cde295b851f24a6a9d5fe
atusy <30277794+atusy@users.noreply.github.com>
2024-05-30 21:19:52 +0900

    feat(fish): stdout fish history after zsh history

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -42,11 +42,16 @@ bind \t complete-and-search
 
 function search_history
   begin
+    history --null | string collect
     if test -f ~/.zsh_history
-      cat ~/.zsh_history | perl -ne 'chomp; if (s/\\\\$//) {print "$_\n"} else {print "$_\0"}'
+      echo -n -e '\0'
+      cat ~/.zsh_history \
+        | perl -ne 'chomp; if (s/\\\\$//) {print "$_\0"} else {print "$_\n"}' \
+        | tac \
+        | perl -pe 's/\0/\\\\\n/g' \
+        | perl -ne 'chomp; if (s/\\\\$//) {print "$_\n"} else {print "$_\0"}'
     end
-    history --reverse --null
-  end | fzf --no-sort --exact --tac --read0 --query=$argv[1]
+  end | fzf --no-sort --exact --read0 --query=$argv[1]
 end
 
 function set_commandline_from_history
