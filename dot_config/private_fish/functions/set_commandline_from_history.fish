337bc7c16d600e3c1b8d9160ead466fb71a25703
atusy <30277794+atusy@users.noreply.github.com>
2024-05-30 22:30:16 +0900

    comment(fish): set_commandline_from_history

--- dot_config/private_fish/functions/set_commandline_from_history.fish
+++ dot_config/private_fish/functions/set_commandline_from_history.fish
@@ -1,4 +1,4 @@
-function set_commandline_from_history
+function set_commandline_from_history --description "A replacement of history-pager"
   set -l cbuf ( commandline -b $buf )
   if set -l nbuf ( search_history --query=$cbuf ); and test -n $nubf
     commandline -r $nbuf
