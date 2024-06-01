aab74f92ff4d4f57159df5ca44e8779b7044f7df
atusy <30277794+atusy@users.noreply.github.com>
2024-06-01 23:21:36 +0900

    fix(fish): test -n/-z should quote variable

--- dot_config/private_fish/functions/fish_prompt.fish
+++ dot_config/private_fish/functions/fish_prompt.fish
@@ -46,14 +46,14 @@ function fish_prompt --description 'Write out the prompt'
     set -l statusb_color (set_color $bold_flag $fish_color_status)
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-    if ! test -z $SSH_CLIENT
+    if test -n "$SSH_CLIENT"
       echo -n -s (prompt_login)' '
     end
     echo -n -s (set_color $color_cwd) (prompt_pwd -D 3) $normal (fish_vcs_prompt)
 
     set -l kubeinfo ( get_kubeinfo2 | string split ' ' ) # faster than get_kubeinfo
     if test -n "$kubeinfo[1]"
-      test -z $kubeinfo[2]; and set kubeinfo[2] 'N/A'
+      test -z "$kubeinfo[2]"; and set kubeinfo[2] 'N/A'
       echo -n -s " [" (set_color cyan) $kubeinfo[1] $normal ":" (set_color cyan) $kubeinfo[2] $normal "]"
     end
     echo -s $normal " "$prompt_status
