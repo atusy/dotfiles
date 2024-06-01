00d4f9e199ec0a3c48a6c50d9f66be1f4114eafa
atusy <30277794+atusy@users.noreply.github.com>
2024-06-01 23:15:44 +0900

    refactor(fish): get kube info with gojq instead of kubectl

--- dot_config/private_fish/functions/fish_prompt.fish
+++ dot_config/private_fish/functions/fish_prompt.fish
@@ -17,56 +17,6 @@ set -g ___fish_git_prompt_char_upstream_diverged '<>'
 set -g ___fish_git_prompt_char_upstream_equal '='
 set -g ___fish_git_prompt_char_upstream_prefix ''
 
-set -g __kube_config
-set -g __kube_ctx
-set -g __kube_ns
-set -g __kube_ts 0
-
-function __update_kubeinfo
-  # normalize KUBECONFIG
-  if set -qx KUBECONFIG; and string match -q -r '^~/' "$KUBECONFIG"
-    eval "set -gx KUBECONFIG $KUBECONFIG"
-  end
-
-  # update if KUBECONFIG path has changed
-  set -l __kube_config_default ~/.kube/config
-  if set -qx KUBECONFIG; and test -n "$KUBECONFIG"
-    if not test "$__kube_config" = "$KUBECONFIG"
-      set __kube_config "$KUBECONFIG"
-      set __kube_ts 0
-      set __kube_ctx
-      set __kube_ns
-    end
-  else if not test "$__kube_config" = "$__kube_config_default"
-    set __kube_config "$__kube_config_default"
-    set __kube_ts 0
-    set __kube_ctx
-    set __kube_ns
-  end
-
-  # abort if KUBECONFIG is not found
-  if not test -f $__kube_config
-    return 1
-  end
-
-  # update if KUBECONFIG content has updated
-  set -l ts ( stat -c "%Y" "$__kube_config" )
-  if test "$ts" -eq "$__kube_ts"
-    return 0
-  end
-  set __kube_ts $ts
-
-  # get ctx
-  set __kube_ctx (kubectl config current-context 2>/dev/null)
-  if test -z "$__kube_ctx"
-    return 1
-  end
-
-  # get ns
-  set __kube_ns (kubectl config view -o "jsonpath={.contexts[?(@.name==\"$__kube_ctx\")].context.namespace}")
-  test -z $__kube_ns; and set __kube_ns 'N/A'
-end
-
 function fish_prompt --description 'Write out the prompt'
     set -l last_pipestatus $pipestatus
     set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
@@ -101,9 +51,10 @@ function fish_prompt --description 'Write out the prompt'
     end
     echo -n -s (set_color $color_cwd) (prompt_pwd -D 3) $normal (fish_vcs_prompt)
 
-    __update_kubeinfo
-    if test -n "$__kube_ctx"
-      echo -n -s " [" (set_color cyan) $__kube_ctx $normal ":" (set_color cyan) $__kube_ns $normal "]"
+    set -l kubeinfo ( get_kubeinfo2 | string split ' ' ) # faster than kubeinfo
+    if test -n "$kubeinfo[1]"
+      test -z $kubeinfo[2]; and set kubeinfo[2] 'N/A'
+      echo -n -s " [" (set_color cyan) $kubeinfo[1] $normal ":" (set_color cyan) $kubeinfo[2] $normal "]"
     end
     echo -s $normal " "$prompt_status
     echo -n -s $suffix " " $normal
---
+++ dot_config/private_fish/functions/get_kubeinfo.fish
@@ -0,0 +1,54 @@
+set -g __kube_config
+set -g __kube_ctx
+set -g __kube_ns
+set -g __kube_ts 0
+
+function __update_kubeinfo
+  # normalize KUBECONFIG
+  if set -qx KUBECONFIG; and string match -q -r '^~/' "$KUBECONFIG"
+    eval "set -gx KUBECONFIG $KUBECONFIG"
+  end
+
+  # update if KUBECONFIG path has changed
+  set -l __kube_config_default ~/.kube/config
+  if set -qx KUBECONFIG; and test -n "$KUBECONFIG"
+    if not test "$__kube_config" = "$KUBECONFIG"
+      set __kube_config "$KUBECONFIG"
+      set __kube_ts 0
+      set __kube_ctx
+      set __kube_ns
+    end
+  else if not test "$__kube_config" = "$__kube_config_default"
+    set __kube_config "$__kube_config_default"
+    set __kube_ts 0
+    set __kube_ctx
+    set __kube_ns
+  end
+
+  # abort if KUBECONFIG is not found
+  if not test -f $__kube_config
+    return 1
+  end
+
+  # update if KUBECONFIG content has updated
+  set -l ts ( stat -c "%Y" "$__kube_config" )
+  if test "$ts" -eq "$__kube_ts"
+    return 0
+  end
+  set __kube_ts $ts
+
+  # get ctx
+  set __kube_ctx (kubectl config current-context 2>/dev/null)
+  if test -z "$__kube_ctx"
+    return 1
+  end
+
+  # get ns
+  set __kube_ns (kubectl config view -o "jsonpath={.contexts[?(@.name==\"$__kube_ctx\")].context.namespace}")
+  test -z $__kube_ns; and set __kube_ns 'N/A'
+end
+
+function get_kubeinfo --description "Get current kubernetes context and namespace with kubectl"
+  __update_kueinfo # use cache for performance
+  echo $__kube_ctx $__kube_ns
+end
