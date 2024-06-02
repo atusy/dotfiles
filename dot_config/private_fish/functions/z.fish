801936ecaeff222ac0d86b7550afee00d08345c3
atusy <30277794+atusy@users.noreply.github.com>
2024-06-02 21:07:11 +0900

    feat(fish): ensure zoxide includes ghq directories

--- dot_config/private_fish/functions/z.fish
+++ dot_config/private_fish/functions/z.fish
@@ -1,4 +1,19 @@
+function __zoxide_list_missing
+  diff \
+    ( zoxide query --list | sort | psub ) \
+    ( ghq list -p | sort | psub ) \
+    | grep '^> ' | string replace -r '^> ' ''
+end
+
+function __zoxide_add_missing
+  set -l missing ( __zoxide_missing )
+  if test ( count $missing ) -gt 0
+    zoxide add $missing
+  end
+end
+
 function z --description 'zoxide wrapper'
+  __zoxide_add_missing
   if test (count $argv) -eq 1
     __zoxide_z $argv || true
   else
