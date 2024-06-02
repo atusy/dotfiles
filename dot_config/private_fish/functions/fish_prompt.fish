d06f4e0c9ba48d3c15e16877aee47077e4d5e4b6
atusy <30277794+atusy@users.noreply.github.com>
2024-06-03 00:05:45 +0900

    feat(fish): use $ as a prompt

--- dot_config/private_fish/functions/fish_prompt.fish
+++ dot_config/private_fish/functions/fish_prompt.fish
@@ -26,7 +26,7 @@ function fish_prompt --description 'Write out the prompt'
 
     # Color the prompt differently when we're root
     set -l color_cwd $fish_color_cwd
-    set -l suffix '>'
+    set -l suffix '$'
     if functions -q fish_is_root_user; and fish_is_root_user
         if set -q fish_color_cwd_root
             set color_cwd $fish_color_cwd_root
