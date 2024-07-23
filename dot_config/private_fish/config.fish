6c8bed7a79900ea453d115cc1cc2594266d1a486
atusy <30277794+atusy@users.noreply.github.com>
2024-07-23 10:09:32 +0900

    fix(fish): suppress command not found error message

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -42,7 +42,7 @@ bind \t complete-and-search
 bind \cr set_commandline_from_history
 
 function update_completion
-  set -l cmd ( type --force-path $argv[1] ); or return
+  set -l cmd ( type --force-path $argv[1] 2>/dev/null ); or return
   set -l out $HOME/.config/fish/completions/$argv[1].fish
   if not test $out -nt $cmd # is true when out is not found or older than cmd
     mkdir -p $HOME/.config/fish/completions
