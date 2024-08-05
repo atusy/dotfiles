16364668c74b37b1f1d458b68da0546ae902a7fd
atusy <30277794+atusy@users.noreply.github.com>
2024-08-05 11:54:28 +0900

    chore(nvim): use deno 1.46.4 for denops

--- dot_config/nvim/lua/plugins/denops/init.lua
+++ dot_config/nvim/lua/plugins/denops/init.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"https://github.com/vim-denops/denops.vim",
 		init = function(p)
-			local bin, cache = require("plugins.denops.utils").get_deno("1.43.5")
+			local bin, cache = require("plugins.denops.utils").get_deno("1.45.4")
 			vim.g["denops#deno"] = bin
 			vim.g["denops#deno_dir"] = cache
 			vim.g["denops#server#deno_args"] = { "-q", "--no-lock", "-A", "--unstable-kv" }
