b537bc12d39e9c9de9a83ac585852e1dcdc292d9
atusy <30277794+atusy@users.noreply.github.com>
2024-08-11 18:29:37 +0900

    fix(fish): remove unexpected argument to commandline -b

--- dot_config/private_fish/functions/set_commandline_from_history.fish
+++ dot_config/private_fish/functions/set_commandline_from_history.fish
@@ -1,5 +1,5 @@
 function set_commandline_from_history --description "A replacement of history-pager"
-  set -l cbuf ( commandline -b $buf )
+  set -l cbuf ( commandline -b )
   if set -l nbuf ( search_history --query=$cbuf ); and test -n $nubf
     commandline -r $nbuf
   end
