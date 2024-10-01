b6d374f6575bb4b51c700c54680e82522a510e73
atusy <30277794+atusy@users.noreply.github.com>
2024-10-01 23:36:43 +0900

    feat(nix): add neovim-nightly

--- dot_config/home-manager/flake.nix
+++ dot_config/home-manager/flake.nix
@@ -8,19 +8,33 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      overlays = [ inputs.neovim-nightly-overlay.overlays.default ];
     in {
       homeConfigurations."atusy" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [
+          ./home.nix
+          {
+            nixpkgs.overlays = overlays;
+            programs.neovim = {
+              enable = true;
+              package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
+            };
+          }
+        ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
