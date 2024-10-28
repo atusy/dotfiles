3fe9ae9a82af444d06241ff3372882dd674db225
atusy <30277794+atusy@users.noreply.github.com>
2024-10-28 10:09:49 +0900

    feat(nvim): add denippet

---
+++ dot_config/nvim/lua/plugins/denippet/init.lua
@@ -0,0 +1,11 @@
+return {
+	"https://github.com/uga-rosa/denippet.vim",
+	dependencies = { "https://github.com/vim-denops/denops.vim" },
+	config = function()
+		local snippetsdir = vim.fs.joinpath(vim.fs.dirname(debug.getinfo(1, "S").source:sub(2)), "snippets")
+		local function load(file, filetypes)
+			vim.fn["denippet#load"](vim.fs.joinpath(snippetsdir, file), filetypes)
+		end
+		load("markdown.toml", { "markdown" })
+	end,
+}
