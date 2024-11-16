739678cf2cbe0845c6db635d3b9fb70d51815907
atusy <30277794+atusy@users.noreply.github.com>
2024-11-17 00:25:46 +0900

    feat!(nvim): remove avante-related plugins

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -747,31 +747,8 @@ return {
 			})
 		end,
 	},
-	{
-		-- support for image pasting in avante
-		"https://github.com/HakonHarnes/img-clip.nvim",
-		event = "VeryLazy",
-		opts = {
-			default = {
-				embed_image_as_base64 = false,
-				prompt_for_file_name = false,
-				drag_and_drop = { insert_mode = true },
-				use_absolute_path = true, -- for windows
-			},
-		},
-	},
 
 	-- filetype specific
-	{
-		"https://github.com/MeanderingProgrammer/render-markdown.nvim",
-		ft = {
-			-- "markdown", -- too noisy
-			"Avante",
-		},
-		config = function(p)
-			require("render-markdown")({ file_types = p.ft })
-		end,
-	},
 	{
 		"https://github.com/barrett-ruth/import-cost.nvim",
 		ft = { "javascript", "typescript", "typescriptreact" },
