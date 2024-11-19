35f611a2628ee9323dc97319b9c408966cb44b8d
atusy <30277794+atusy@users.noreply.github.com>
2024-11-19 10:08:46 +0900

    feat!(nix): do not install R

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -72,27 +72,27 @@
     pkgs.stylua
 
     # R
-    (pkgs.radianWrapper.override {
-      packages = with pkgs.rPackages; [
-        clock
-        ragg
-        blogdown
-        devtools
-        DBI
-        dbplyr
-        duckdb
-        felp
-        pak
-        tidyverse
-        renv
-        shiny
-        styler
-        testit
-        testthat
-        languageserver
-      ];
-      wrapR = true;
-    })
+    # (pkgs.radianWrapper.override {
+    #   packages = with pkgs.rPackages; [
+    #     clock
+    #     ragg
+    #     blogdown
+    #     devtools
+    #     DBI
+    #     dbplyr
+    #     duckdb
+    #     felp
+    #     pak
+    #     tidyverse
+    #     renv
+    #     shiny
+    #     styler
+    #     testit
+    #     testthat
+    #     languageserver
+    #   ];
+    #   wrapR = true;
+    # })
 
     # Fonts
     pkgs.udev-gothic
