c90bf9586acd2219fb19ccacbad5a155fa0ad7d4
atusy <30277794+atusy@users.noreply.github.com>
2024-11-26 14:03:11 +0900

    feat(fish): set title relative to git top level dir

--- dot_config/private_fish/functions/fish_title.fish
+++ dot_config/private_fish/functions/fish_title.fish
@@ -3,7 +3,16 @@ function fish_title
     set -l ssh
     set -q SSH_TTY
     and set ssh "["(prompt_hostname | string sub -l 10 | string collect)"]"
-    echo -- $ssh ( basename $PWD )
+
+    set -l wd
+    if set -l gwd (git rev-parse --show-toplevel 2> /dev/null)
+      set -l n (dirname "xx$gwd" | string length) # add extra characters to generate start index
+      set wd (string sub --start $n $PWD)
+    else
+      set wd (basename $PWD)
+    end
+
+    echo -- $ssh $wd
   end
 end
 
