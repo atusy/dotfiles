955501155690e65aa9e042629e674ad33bfc0d7f
atusy <30277794+atusy@users.noreply.github.com>
2024-12-01 23:12:36 +0900

    fix(nvim): <c-s> not working properly on ginedit

--- dot_config/nvim/lua/atusy/init.lua
+++ dot_config/nvim/lua/atusy/init.lua
@@ -147,8 +147,14 @@ end, { expr = true })
 set_keymap({ "n", "x" }, "<Plug>(save)", function()
 	---@diagnostic disable-next-line: undefined-field
 	local nm = vim.api.nvim_buf_get_name(0)
-	for prefix in ipairs({ "term://", "ginedit://" }) do
-		if nm:match("^" .. prefix) then
+	for prefix in ipairs({ "term://" }) do
+		if nm:sub(1, #prefix) == prefix then
+			return
+		end
+	end
+	for _, prefix in ipairs({ "ginedit://" }) do
+		if nm:sub(1, #prefix) == prefix then
+			vim.cmd.w()
 			return
 		end
 	end
