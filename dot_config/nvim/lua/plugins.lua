99b0fa18e565daa424f94857c0b579c57ad45117
atusy <30277794+atusy@users.noreply.github.com>
2024-12-27 00:41:15 +0900

    comment(nvim): add comments to enhanced W-motion

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -27,7 +27,7 @@ return {
 				local line = vim.api.nvim_buf_get_lines(0, cursor[1] - 1, cursor[1], false)[1]
 				local right = line:sub(cursor[2] + 1)
 
-				-- if cursor is on %s, %w, or %p
+				--[[ if cursor is on %s, %w, or %p ]]
 				local i = 0
 				for char in right:gmatch(".") do
 					i = i + 1
@@ -53,7 +53,7 @@ return {
 					end
 				end
 
-				-- jump with budoux
+				-- [[jump with budoux]]
 				-- if there is no break within the sequence of multibyte characters,
 				-- jump to the end of the seqience.
 				--
@@ -69,17 +69,24 @@ return {
 						break
 					end
 				end
+
 				local w, s, p, r = right:find("%w"), right:find("%s"), right:find("%p"), #right
 				local delta = math.min(w or r, s or r, p or r)
+
+				-- jump to the next segment
 				local nmax = cursor[2] + delta - 1
 				if n < nmax or (nmax - cursor[2]) <= 1 then
 					vim.api.nvim_win_set_cursor(0, { cursor[1], n })
 					return
 				end
+
+				-- jump to the next WORD
 				if delta == s then
 					vim.cmd("normal! W")
 					return
 				end
+
+				-- jump to the next %w or %p or the end of the line
 				vim.api.nvim_win_set_cursor(0, { cursor[1], nmax })
 
 				-- jump to next line if cursor is on the last character
