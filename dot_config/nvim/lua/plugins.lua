771b23bcb29b8b83037fa75eaa435c819e9ef21d
atusy <30277794+atusy@users.noreply.github.com>
2025-03-08 00:08:36 +0900

    feat(nvim): add budouxify.nvim

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -18,87 +18,6 @@ vim.api.nvim_create_autocmd("User", {
 })
 
 return {
-	{
-		"https://github.com/atusy/budoux.lua",
-		dev = true,
-		init = function()
-			vim.keymap.set({ "n", "x", "o" }, "W", function()
-				local cursor = vim.api.nvim_win_get_cursor(0)
-				local line = vim.api.nvim_buf_get_lines(0, cursor[1] - 1, cursor[1], false)[1]
-				local right = line:sub(cursor[2] + 1)
-
-				--[[ if cursor is on %s, %w, or %p ]]
-				local i = 0
-				for char in right:gmatch(".") do
-					i = i + 1
-
-					if char:match("%s") then
-						-- jump with W if
-						-- * cursor is on a space
-						-- * cursor is on a sequence of alphanumerics or punctuations followed by a space
-						vim.cmd("normal! W")
-						return
-					end
-
-					if not char:match("%w") and not char:match("%p") then
-						if i == 1 then
-							-- jump with budoux if cursor is on a multibyte character
-							break
-						end
-
-						-- jump to the next multibyte character
-						-- if cursor is on a sequence of alphanumerics or punctuations followed by a multibyte character
-						vim.api.nvim_win_set_cursor(0, { cursor[1], cursor[2] + i - 1 })
-						return
-					end
-				end
-
-				-- [[jump with budoux]]
-				-- if there is no break within the sequence of multibyte characters,
-				-- jump to the end of the seqience.
-				--
-				-- Example where `|` is the cursor and `^` is the jump target:
-				-- あああa
-				-- |     ^
-				local model = require("budoux").load_japanese_model()
-				-- Segumentation requires characters on the left of the cursor.
-				-- TODO:to improve performance, do segmentation within a sentence or between singlebyte characters (%w, %s, %p).
-				local segments = model.parse(line)
-				local n = 0
-				for _, seg in ipairs(segments) do
-					n = n + #seg
-					if n > cursor[2] then
-						break
-					end
-				end
-
-				local w, s, p, r = right:find("%w"), right:find("%s"), right:find("%p"), #right
-				local delta = math.min(w or r, s or r, p or r)
-
-				-- jump to the next segment
-				local nmax = cursor[2] + delta - 1
-				if n < nmax then
-					vim.api.nvim_win_set_cursor(0, { cursor[1], n })
-					return
-				end
-
-				-- jump to the next WORD
-				if delta == s then
-					vim.cmd("normal! W")
-					return
-				end
-
-				-- jump to the next %w or %p or the end of the line
-				vim.api.nvim_win_set_cursor(0, { cursor[1], nmax })
-
-				-- jump to next line if cursor is on the last character
-				vim.cmd("normal! hl") -- correct cursor position if it is on the middle of a multibyte character
-				if vim.api.nvim_win_get_cursor(0)[2] == cursor[2] then
-					vim.cmd("normal! W")
-				end
-			end)
-		end,
-	},
 	-- basic dependencies
 	{ "https://github.com/nvim-lua/plenary.nvim", lazy = true },
 	{
@@ -392,6 +311,38 @@ return {
 	},
 
 	-- motion
+	{
+		"https://github.com/atusy/budoux.lua",
+		dev = true,
+		lazy = true,
+	},
+	{
+		"https://github.com/atusy/budouxify.nvim",
+		dev = true,
+		lazy = true,
+		init = function()
+			vim.keymap.set("n", "W", function()
+				local pos = require("budouxify.motion").find_forward({
+					row = vim.api.nvim_win_get_cursor(0)[1],
+					col = vim.api.nvim_win_get_cursor(0)[2],
+					head = true,
+				})
+				if pos then
+					vim.api.nvim_win_set_cursor(0, { pos.row, pos.col })
+				end
+			end)
+			vim.keymap.set("n", "E", function()
+				local pos = require("budouxify.motion").find_forward({
+					row = vim.api.nvim_win_get_cursor(0)[1],
+					col = vim.api.nvim_win_get_cursor(0)[2],
+					head = false,
+				})
+				if pos then
+					vim.api.nvim_win_set_cursor(0, { pos.row, pos.col })
+				end
+			end)
+		end,
+	},
 	{
 		"https://github.com/atusy/jab.nvim",
 		dev = true,
