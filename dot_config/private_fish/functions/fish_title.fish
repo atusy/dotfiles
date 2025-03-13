d96cfcc4e454976387f075d57f32b46d84cc43b4
atusy <30277794+atusy@users.noreply.github.com>
2025-03-14 01:12:23 +0900

    feat(fish): update fish_title

--- dot_config/private_fish/functions/fish_title.fish
+++ dot_config/private_fish/functions/fish_title.fish
@@ -1,18 +1,23 @@
+set -l _fish_title_pwd
+set -l _fish_title_wd
+
 function fish_title
   if not set -q INSIDE_EMACS; or string match -vq '*,term:*' -- $INSIDE_EMACS
     set -l ssh
     set -q SSH_TTY
     and set ssh "["(prompt_hostname | string sub -l 10 | string collect)"]"
 
-    set -l wd
-    if set -l gwd (git rev-parse --show-toplevel 2> /dev/null)
-      set -l n (dirname "xx$gwd" | string length) # add extra characters to generate start index
-      set wd (string sub --start $n $PWD)
-    else
-      set wd (basename $PWD)
+    if test "$_fish_title_pwd" != "$PWD"
+      set _fish_title_pwd $PWD
+      if set -l gwd (git rev-parse --show-toplevel 2> /dev/null)
+        set -l n (dirname "xx$gwd" | string length) # add extra characters to generate start index
+        set _fish_title_wd (string sub --start $n $PWD)
+      else
+        set _fish_title_wd (basename $PWD)
+      end
     end
 
-    echo -- $ssh $wd
+    echo -- $ssh $_fish_title_wd
   end
 end
 
