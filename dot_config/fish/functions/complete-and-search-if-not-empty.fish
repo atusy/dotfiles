b73d5a2f236eac369d33c43448e31c31043d2be9
atusy <30277794+atusy@users.noreply.github.com>
2025-03-21 23:00:24 +0900

    fix(fish): complete-and-search-if-not-empty should consider the multiline prompt

--- dot_config/fish/functions/complete-and-search-if-not-empty.fish
+++ dot_config/fish/functions/complete-and-search-if-not-empty.fish
@@ -1,5 +1,5 @@
 function complete-and-search-if-not-empty
-  set -l n (commandline -b $buf | string length)
+  set -l n (commandline -b $buf | string join0 | string length)
   if test $n -gt 0
     commandline -f complete-and-search
   end
