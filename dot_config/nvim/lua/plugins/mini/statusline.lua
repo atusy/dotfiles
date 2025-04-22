29e2892113fdea615fa63adbfe1bcae3b4036fc5
atusy <30277794+atusy@users.noreply.github.com>
2025-04-22 20:24:23 +0900

    feat(nvim): simplify statusline

--- dot_config/nvim/lua/plugins/mini/statusline.lua
+++ dot_config/nvim/lua/plugins/mini/statusline.lua
@@ -1,9 +1,8 @@
 local MiniStatusline = require("mini.statusline")
 
 local function active()
-	local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+	local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = math.huge })
 	local filename = MiniStatusline.section_filename({ trunc_width = 140 })
-	local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
 	local location = MiniStatusline.section_location({ trunc_width = 75 })
 	local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
 
@@ -12,7 +11,6 @@ local function active()
 		"%<", -- Mark general truncate point
 		{ hl = "MiniStatuslineFilename", strings = { filename } },
 		"%=", -- End left alignment
-		{ hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
 		{ hl = mode_hl, strings = { search, location } },
 	})
 end
