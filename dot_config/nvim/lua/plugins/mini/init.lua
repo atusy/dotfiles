cb91b9b7c0b828c26172d5fb698ed9a5897080ea
atusy <30277794+atusy@users.noreply.github.com>
2025-04-22 20:46:19 +0900

    fix(nvim): fix typo

--- dot_config/nvim/lua/plugins/mini/init.lua
+++ dot_config/nvim/lua/plugins/mini/init.lua
@@ -13,7 +13,7 @@ return {
 			require("plugins.mini.statusline").lazy()
 		end,
 		config = function()
-			-- lazy load bini.bufremove
+			-- lazy load mini.bufremove
 			local bufremove = [[<Cmd>lua require("mini.bufremove").%s()<CR>]]
 			require("atusy.keymap.palette").add_item("n", "Bdelete", bufremove:format("delete"))
 			require("atusy.keymap.palette").add_item("n", "Bwipeout", bufremove:format("wipeout"))
