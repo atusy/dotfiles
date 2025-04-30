23501e17aa17b0ef110ed4fbf822489cb367046b
atusy <30277794+atusy@users.noreply.github.com>
2025-04-30 21:21:08 +0900

    feat(nvim): add keymap to clipboard URL to the help tag in neovim.io

--- dot_config/nvim/after/ftplugin/help.lua
+++ dot_config/nvim/after/ftplugin/help.lua
@@ -1,5 +1,8 @@
 pcall(vim.treesitter.start)
 vim.wo[0].conceallevel = 0
+local bufname = vim.api.nvim_buf_get_name(0)
+local bufdir = vim.fs.dirname(bufname)
+local is_git_repo = vim.system({ "git", "rev-parse", "--is-inside-work-tree" }, { cwd = bufdir }):wait().code == 0
 
 local function wincmd_L()
 	local wins = vim.api.nvim_tabpage_list_wins(0)
@@ -40,3 +43,79 @@ vim.keymap.set("n", "K", function()
 	end
 	return "gF"
 end, { expr = true, buffer = true, desc = "Go to definition, open url, or open file" })
+
+---@param node TSNode
+---@return TSNode | nil
+local function find_tag_in_decendant(node)
+	for child in node:iter_children() do
+		if child:type() == "tag" then
+			return child
+		end
+		local descendant = find_tag_in_decendant(child)
+		if descendant then
+			return descendant
+		end
+	end
+end
+
+---@param node TSNode
+---@return TSNode | nil
+local function find_tag(node)
+	local node_ancestor = node ---@type TSNode
+
+	-- Find the nearest ancestor that is a block or tag
+	-- if tag, return it
+	-- if block, break the loop
+	while true do
+		if node_ancestor:type() == "tag" then
+			return node_ancestor
+		end
+		if node_ancestor:type() == "block" then
+			break
+		end
+		local node_parent = node_ancestor:parent()
+		if not node_parent then
+			break
+		end
+		node_ancestor = node_parent
+	end
+
+	-- find tag in the currrent block or the previous siblings of the block
+	local node_block = node_ancestor ---@type TSNode | nil
+	while true do
+		if not node_block then
+			break
+		end
+		local node_tag = find_tag_in_decendant(node_block)
+		if node_tag then
+			return node_tag
+		end
+		node_block = node_block:prev_named_sibling()
+	end
+end
+
+if not is_git_repo then
+	-- otherwise, use global mapping defined at ~/.config/nvim/lua/plugins/git/init.lua
+	vim.keymap.set("n", "<Plug>(C-G)<C-Y>", function()
+		local node_cursor = vim.treesitter.get_node()
+		if node_cursor == nil then
+			return
+		end
+		local node_tag = find_tag(node_cursor)
+
+		if not node_tag then
+			return
+		end
+
+		local node_text = vim.treesitter.get_node_text(node_tag, 0, {}):gsub("^[*]", ""):gsub("[*]$", "")
+		local file_name = vim.fs.basename(vim.api.nvim_buf_get_name(0)):gsub("[.]txt$", "")
+		local url = string.format(
+			-- e.g., https://neovim.io/doc/user/editing.html#CTRL-G
+			"https://neovim.io/doc/user/%s.html#%s",
+			file_name,
+			vim.uri_encode(node_text):gsub(":", "%%3A")
+		)
+		vim.fn.setreg("+", url)
+		vim.notify(url)
+	end, { buffer = true })
+end
