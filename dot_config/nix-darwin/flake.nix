c58c2ba1564b82a7f0fe14f93422ff1472e5ad43
atusy <30277794+atusy@users.noreply.github.com>
2025-05-02 22:53:28 +0900

    feat(darwin): add neovim-nightly

--- dot_config/nix-darwin/flake.nix
+++ dot_config/nix-darwin/flake.nix
@@ -5,15 +5,19 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, ... }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.vim
+        [ pkgs.neovim
         ];
 
       # Necessary for using flakes on this system.
@@ -30,6 +34,7 @@
       system.stateVersion = 6;
 
       nixpkgs.hostPlatform = "aarch64-darwin";
+      nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlays.default ];
     };
     darwinHost = builtins.getEnv "DARWIN_HOST";
   in
