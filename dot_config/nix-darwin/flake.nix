988a10216ab8af4ee19f9a50106f7ff48bbbf83d
atusy <30277794+atusy@users.noreply.github.com>
2025-05-02 21:20:01 +0900

    feat(darwin): add flake.nix

---
+++ dot_config/nix-darwin/flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.vim
+        ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+    darwinHost = builtins.getEnv "DARWIN_HOST";
+  in
+  {
+    darwinConfigurations.${darwinHost} = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+  };
+}
