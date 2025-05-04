347b27adc31c04d0f475cf420740562880849d58
atusy <30277794+atusy@users.noreply.github.com>
2025-05-04 21:44:17 +0900

    feat(nix): merge home-manager and nix-darwin into nix-flake

--- dot_config/nix-flake/flake.nix
+++ dot_config/nix-flake/flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -17,30 +19,62 @@
   outputs =
     inputs@{
       self,
+      nix-darwin,
       nixpkgs,
       home-manager,
       ...
     }:
     let
-      system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       overlays = [ inputs.neovim-nightly-overlay.overlays.default ];
+      darwinConfiguration =
+        { pkgs, ... }:
+        {
+          environment.systemPackages = (import ./pkgs.nix { inherit pkgs; }).system;
+          fonts.packages = (import ./pkgs.nix { inherit pkgs; }).fonts;
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Enable alternative shell support in nix-darwin.
+          # programs.fish.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
+
+          nixpkgs.hostPlatform = "aarch64-darwin";
+          nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlays.default ];
+          nixpkgs.config.allowUnfree = true;
+        };
+      darwinHost = builtins.getEnv "DARWIN_HOST";
+      system = if darwinHost == "" then "x86_64-linux" else "aarch64-darwin";
     in
-    {
-      homeConfigurations."atusy" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+    if true then
+      {
+        homeConfigurations."atusy" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [
-          {
-            nixpkgs.overlays = overlays;
-          }
-          ./home.nix
-        ];
+          # Specify your home configuration modules here, for example,
+          # the path to your home.nix.
+          modules = [
+            {
+              nixpkgs.overlays = overlays;
+            }
+            ./home.nix
+          ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+          # Optionally use extraSpecialArgs
+          # to pass through arguments to home.nix
+        };
+      }
+    else
+      {
+        darwinConfigurations.${darwinHost} = nix-darwin.lib.darwinSystem {
+          modules = [ darwinConfiguration ];
+        };
       };
-    };
 }
---
+++ dot_config/nix-flake/pkgs.nix
@@ -0,0 +1,77 @@
+{ pkgs, ... }:
+
+{
+  system = [
+    pkgs.aha # Converet ANSI escape sequences to HTML
+    pkgs.avahi
+    pkgs.bash
+    pkgs.bat
+    pkgs.chezmoi
+    pkgs.delta
+    pkgs.deno
+    pkgs.direnv
+    pkgs.duf
+    pkgs.gdu
+    pkgs.htop
+    pkgs.eza
+    pkgs.go
+    pkgs.go-task
+    pkgs.fd
+    pkgs.fish
+    pkgs.fzf
+    pkgs.google-cloud-sdk
+    pkgs.gh
+    pkgs.ghq
+    pkgs.git
+    pkgs.gojq
+    pkgs.jq
+    pkgs.kind
+    pkgs.kubectl
+    pkgs.kubectx
+    pkgs.mise
+    pkgs.mypy
+    pkgs.neovim # nightly
+    pkgs.nodejs
+    pkgs.pandoc
+    pkgs.pnpm
+    pkgs.podman
+    pkgs.podman-compose
+    pkgs.procs
+    pkgs.python312
+    pkgs.ripgrep
+    pkgs.rsync
+    pkgs.terraform
+    pkgs.tmux
+    pkgs.trash-cli
+    pkgs.uv
+    pkgs.wget2
+    pkgs.zoxide
+
+    # language servers
+    pkgs.gopls
+    pkgs.lua-language-server
+    pkgs.nil
+    pkgs.nodePackages_latest.bash-language-server
+    pkgs.nodePackages_latest.vscode-json-languageserver
+    pkgs.pyright
+    pkgs.typescript-language-server
+    pkgs.terraform-ls
+    pkgs.yaml-language-server
+
+    # formatters/linters
+    pkgs.actionlint
+    pkgs.biome
+    pkgs.checkmate
+    pkgs.nixfmt-rfc-style
+    pkgs.ruff
+    pkgs.shellcheck
+    pkgs.shfmt
+    pkgs.stylua
+  ];
+  fonts = [
+    pkgs.udev-gothic
+    pkgs.udev-gothic-nf
+    pkgs.noto-fonts-color-emoji
+    pkgs.ibm-plex
+  ];
+}
