22f5b074b568a65895bafe93e38199e07294c24a
atusy <30277794+atusy@users.noreply.github.com>
2025-05-12 22:19:37 +0900

    feat(nvim): show macro register in statusline

--- dot_config/nvim/lua/plugins/mini/statusline.lua
+++ dot_config/nvim/lua/plugins/mini/statusline.lua
@@ -1,6 +1,22 @@
 local M = {}
 
-local function active()
+local macro_register = nil
+
+local function on_macro_recording(ctx)
+	if vim.o.cmdheight > 0 then
+		-- register is shown in cmdline
+		return
+	end
+	macro_register = vim.fn.reg_recording()
+	vim.api.nvim_create_autocmd("RecordingLeave", {
+		group = ctx and ctx.group or vim.api.nvim_create_augroup("atusy-mini-statusline-recording-leave", {}),
+		callback = function()
+			macro_register = nil
+		end,
+	})
+end
+
+local function make_active_statusline()
 	local MiniStatusline = require("mini.statusline")
 	local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = math.huge })
 	local filename = MiniStatusline.section_filename({ trunc_width = math.huge }) -- prefer relative path
@@ -8,6 +24,7 @@ local function active()
 	local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
 
 	return MiniStatusline.combine_groups({
+		{ hl = "MiniStatuslineDevinfo", strings = { macro_register } },
 		{ hl = mode_hl, strings = { mode } },
 		"%<", -- Mark general truncate point
 		{ hl = "MiniStatuslineFilename", strings = { filename } },
@@ -20,13 +37,18 @@ function M.setup()
 	local MiniStatusline = require("mini.statusline")
 	MiniStatusline.setup({
 		content = {
-			active = active,
+			active = make_active_statusline,
 			inactive = function()
 				return MiniStatusline.combine_groups({
 					MiniStatusline.section_filename({ trunc_width = math.huge }), -- prefer relative path
 				})
 			end,
 		},
+	})
+
+	vim.api.nvim_create_autocmd("RecordingEnter", {
+		group = vim.api.nvim_create_augroup("atusy-mini-statusline-recording", {}),
+		callback = on_macro_recording,
 	})
 end
 
@@ -42,6 +64,7 @@ function M.lazy()
 			vim.keymap.del("n", "n")
 			vim.keymap.del("n", "N")
 		end)
+		loaded = true
 	end
 
 	-- lazy load with n/N to show search count
@@ -55,12 +78,19 @@ function M.lazy()
 	end, { expr = true })
 
 	-- lazy load with WinNew to show window status
-	vim.api.nvim_create_autocmd("WinNew", {
+	vim.api.nvim_create_autocmd({ "WinNew", "RecordingEnter" }, {
 		group = vim.api.nvim_create_augroup("atusy-mini-statusline", {}),
-		callback = function()
+		callback = function(ctx)
 			if loaded then
 				return true
 			end
+
+			if ctx.event == "RecordingEnter" and vim.o.cmdheight == 0 then
+				on_macro_recording()
+				setup()
+				return true
+			end
+
 			local cnt = 0
 			for _, w in pairs(vim.api.nvim_tabpage_list_wins(0)) do
 				if vim.api.nvim_win_get_config(w).relative == "" then
