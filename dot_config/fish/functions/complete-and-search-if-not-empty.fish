b089b9fa4a301c83dbc5058c91e19715fc7d9b4e
atusy <30277794+atusy@users.noreply.github.com>
2025-05-13 00:12:13 +0900

    fix(fish): complete-and-search-if-not-empty completes even if empty

--- dot_config/fish/functions/complete-and-search-if-not-empty.fish
+++ dot_config/fish/functions/complete-and-search-if-not-empty.fish
@@ -1,7 +1,8 @@
 function complete-and-search-if-not-empty
   set -l n (commandline -b | string length)
-  if test $n -gt 0
-    commandline -f complete-and-search
+  if test (count $n -eq 1) -a $n -eq 0
+    return
   end
+  commandline -f complete-and-search
 end
 
