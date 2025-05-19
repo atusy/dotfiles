34fe4fc2ec131a7060d3e6bdc857f681da7f209f
atusy <30277794+atusy@users.noreply.github.com>
2025-05-19 09:55:55 +0900

    feat(nvim): support formatting rmd

--- dot_config/nvim/lua/plugins/conform/init.lua
+++ dot_config/nvim/lua/plugins/conform/init.lua
@@ -71,6 +71,7 @@ return {
 					nix = { "nixfmt" },
 					python = { "ruff_format", "ruff_fix" },
 					r = { "air", "styler", stop_after_first = true },
+					rmd = { "air", "styler", stop_after_first = true },
 				},
 			})
 		end,
