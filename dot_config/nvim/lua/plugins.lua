968d4114933cb7e9e687c7a2b00721d08dfaa804
atusy <30277794+atusy@users.noreply.github.com>
2025-06-09 00:00:54 +0900

    refactor(nvim): use treemonkey.filters.linewise

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -445,43 +445,17 @@ return {
 				experimental = { treesitter_context = true },
 			}
 
-			local function select_linewise()
-				require("treemonkey").select(vim.tbl_extend("force", base_opts, {
-					---@param nodes TSNode[]
-					---@return TSNode[]
-					filter = function(nodes)
-						local idx = {} ---@type table<number, table<number, {scol: number, idx: number}>>
-						local res = {} ---@type TSNode[]
-						for _, n in pairs(nodes) do
-							local srow, scol, erow, ecol = n:range()
-							if erow > (srow + (ecol == 0 and 1 or 0)) then
-								if not idx[srow] then
-									table.insert(res, n)
-									idx[srow] = {}
-									idx[srow][erow] = { scol = scol, idx = #res }
-								elseif not idx[srow][erow] then
-									table.insert(res, n)
-									idx[srow][erow] = { scol = scol, idx = #res }
-								elseif scol < idx[srow][erow].scol then
-									res[idx[srow][erow].idx] = n
-									idx[srow][erow].scol = scol
-								end
-							end
-						end
-						return res
-					end,
-				}))
-			end
-
 			vim.keymap.set({ "x", "o" }, "<Plug>(treemonkey)", function()
 				local mode = vim.api.nvim_get_mode()
 				if mode.mode == "V" or mode.mode == "noV" then
-					select_linewise()
+					require("treemonkey").select(vim.tbl_extend("force", base_opts, {
+						filter = require("treemonkey.filters").linewise,
+					}))
 					return
 				end
 				require("treemonkey").select(base_opts)
 			end)
-			vim.keymap.set({ "o", "x" }, "m", "<Plug>(treemonkey)")
+			vim.keymap.set({ "x", "o" }, "m", "<Plug>(treemonkey)")
 			vim.keymap.set("n", "zf", "zfV<Plug>(treemonkey)")
 		end,
 	},
