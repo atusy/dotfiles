617c3e71b5da65f6f3a14d3f0edc91d3f6940fb7
atusy <30277794+atusy@users.noreply.github.com>
2025-07-28 22:39:19 +0900

    feat(nvim): manually install custom treesitter parsers

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,15 @@
 local augroup = vim.api.nvim_create_augroup("atusy.plugins.nvim-treesitter", {})
-local treesitterpath = vim.fn.stdpath("data") .. "/treesitter"
+local treesitter_path = vim.fs.joinpath(vim.fn.stdpath("data"), "/treesitter")
+
+---@param cb fun(parser_path: string): nil
+local function with_parser_dir(cb)
+	local parser_dir = vim.fs.joinpath(treesitter_path, "parser")
+	vim.uv.fs_mkdir(parser_dir, tonumber("755", 8), function()
+		if cb then
+			cb(parser_dir)
+		end
+	end)
+end
 
 return {
 	{
@@ -53,7 +63,7 @@ return {
 			}
 
 			-- setup
-			require("nvim-treesitter").setup({ install_dir = treesitterpath })
+			require("nvim-treesitter").setup({ install_dir = treesitter_path })
 
 			-- register parsers to some other languages
 			vim.treesitter.language.register("bash", "zsh")
@@ -61,7 +71,6 @@ return {
 			vim.treesitter.language.register("json", "jsonl")
 			vim.treesitter.language.register("json", "ndjson")
 			vim.treesitter.language.register("hcl", "tf")
-			vim.treesitter.language.register("unifieddiff", "gin-diff")
 			vim.treesitter.language.register("markdown", "rmd")
 			vim.treesitter.language.register("markdown", "qmd")
 
@@ -75,6 +84,34 @@ return {
 				"ColorScheme",
 				{ group = vim.api.nvim_create_augroup("atusy.nvim-treesitter", {}), callback = hi }
 			)
+		end,
+	},
+	{
+		"https://github.com/monaqa/tree-sitter-unifieddiff",
+		filetype = { "unifieddiff", "gin-diff" },
+		init = function(opts)
+			with_parser_dir(function(parser_path)
+				vim.system(
+					{ "tree-sitter", "build", "--output", vim.fs.joinpath(parser_path, "unifieddiff.so") },
+					{ cwd = opts.dir },
+					function() end
+				)
+			end)
+		end,
+		config = function()
+			vim.treesitter.language.register("unifieddiff", "gin-diff")
+		end,
+	},
+	{
+		"https://github.com/atusy/tree-sitter-uri",
+		build = function(opts)
+			with_parser_dir(function(parser_path)
+				vim.system(
+					{ "tree-sitter", "build", "--output", vim.fs.joinpath(parser_path, "uri.so") },
+					{ cwd = opts.dir },
+					function() end
+				)
+			end)
 		end,
 	},
 }
