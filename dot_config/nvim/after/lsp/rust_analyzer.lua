63c033f73dd775a74fcf893046900a29fe1b4739
atusy <30277794+atusy@users.noreply.github.com>
2025-08-26 22:16:22 +0900

    feat(nvim): update rust-analyzer settings

--- dot_config/nvim/after/lsp/rust_analyzer.lua
+++ dot_config/nvim/after/lsp/rust_analyzer.lua
@@ -1,6 +1,11 @@
 vim.lsp.config.rust_analyzer = {
 	settings = {
 		["rust-analyzer"] = {
+			cargo = {
+				buildScripts = { enable = true },
+				features = "all",
+				imports = { granularity = { enforce = true } },
+			},
 			diagnostics = {
 				enable = true,
 				experimental = {
