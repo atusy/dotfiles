e313416b68b7c193cb7ca7a513e6d2439fa9ee62
atusy <30277794+atusy@users.noreply.github.com>
2025-11-11 09:52:32 +0900

    refactor(fish): make variable scope stricter

--- dot_config/fish/functions/fish_prompt.fish
+++ dot_config/fish/functions/fish_prompt.fish
@@ -57,8 +57,7 @@ function fish_prompt --description 'Write out the prompt'
     set -l prompt_vcs (fish_vcs_prompt)
 
     set -l prompt_kubeinfo
-    set -l kubeinfo ( get_kubeinfo | string split " " )
-    if test -n "$kubeinfo[1]"
+    if set -l kubeinfo ( get_kubeinfo | string split " " ); and test -n "$kubeinfo[1]"
       test -z "$kubeinfo[2]"; and set kubeinfo[2] "N/A"
       set prompt_kubeinfo " ["(set_color cyan){$kubeinfo[1]}{$normal}":"(set_color cyan){$kubeinfo[2]}{$normal}"]"
     end
