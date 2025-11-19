c90acd127a1440699cb6a672634005b401fee4d3
atusy <30277794+atusy@users.noreply.github.com>
2025-11-20 00:22:56 +0900

    fix(nvim): should load denops.vim prior to denippet.vim

--- dot_config/nvim/lua/plugins/denippet/init.lua
+++ dot_config/nvim/lua/plugins/denippet/init.lua
@@ -1,5 +1,9 @@
 return {
 	"https://github.com/uga-rosa/denippet.vim",
+	dependencies = {
+		-- To avoid E117: Unknown function: denops#plugin#wait_async
+		"https://github.com/vim-denops/denops.vim",
+	},
 	config = function()
 		local snippetsdir = vim.fs.joinpath(vim.fs.dirname(debug.getinfo(1, "S").source:sub(2)), "snippets")
 		local function load(file, filetypes)
