366a2d34e61c2a36b7622b7c2bccfa843bd4f409
atusy <30277794+atusy@users.noreply.github.com>
2025-11-21 01:50:28 +0900

    feat(fish): support `git master` to checkout detach master

--- dot_config/fish/functions/__abbr-git-switch-detach.fish
+++ dot_config/fish/functions/__abbr-git-switch-detach.fish
@@ -1,7 +1,7 @@
 function __abbr-git-switch-detach
   set -l buf (commandline --current-process | string trim)
   set -l branch (string match --regex '^git (\S+)\s*$' "$buf")[2]
-  if test "$branch" = 'dev' -o "$branch" = 'main'
+  if test "$branch" = 'dev' -o "$branch" = 'main' -o "$branch" = 'master'
     echo "fetch origin $branch && git switch origin/$branch --detach"
     return 0
   end
