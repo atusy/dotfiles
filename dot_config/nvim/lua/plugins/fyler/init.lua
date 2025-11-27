fdbdc3d22e2875f56d8b24f3f6300603e2fd4e01
atusy <30277794+atusy@users.noreply.github.com>
2025-11-27 20:59:06 +0900

    refactor(nvim): simplify init func

--- dot_config/nvim/lua/plugins/fyler/init.lua
+++ dot_config/nvim/lua/plugins/fyler/init.lua
@@ -1,37 +1,44 @@
+local L = {}
+
+function L.setup_winbar()
+	vim.api.nvim_create_autocmd("BufWinEnter", {
+		callback = function(ctx)
+			if vim.startswith(ctx.file, "fyler://") then
+				if not vim.w.old_winbar then
+					vim.w.old_winbar = vim.wo[0].winbar
+				end
+				vim.wo[0].winbar = "%F"
+				return
+			end
+
+			if vim.w.old_winbar then
+				vim.wo[0].winbar = vim.w.old_winbar
+			end
+		end,
+	})
+end
+
+function L.open()
+	local bufs = vim.api.nvim_list_bufs()
+	for _, buf in ipairs(bufs) do
+		local nm = vim.api.nvim_buf_get_name(buf)
+		if vim.startswith(nm, "fyler://") then
+			vim.api.nvim_win_set_buf(0, buf)
+			return
+		end
+	end
+	if vim.v.count > 0 then
+		require("fyler").close()
+	end
+	require("fyler").open()
+end
+
 return {
 	{
 		"https://github.com/A7Lavinraj/fyler.nvim",
 		init = function()
-			vim.api.nvim_create_autocmd("BufWinEnter", {
-				callback = function(ctx)
-					if vim.startswith(ctx.file, "fyler://") then
-						if not vim.w.old_winbar then
-							vim.w.old_winbar = vim.wo[0].winbar
-						end
-						vim.wo[0].winbar = "%F"
-						return
-					end
-
-					if vim.w.old_winbar then
-						vim.wo[0].winbar = vim.w.old_winbar
-					end
-				end,
-			})
-
-			vim.keymap.set("n", "S", function()
-				local bufs = vim.api.nvim_list_bufs()
-				for _, buf in ipairs(bufs) do
-					local nm = vim.api.nvim_buf_get_name(buf)
-					if vim.startswith(nm, "fyler://") then
-						vim.api.nvim_win_set_buf(0, buf)
-						return
-					end
-				end
-				if vim.v.count > 0 then
-					require("fyler").close()
-				end
-				require("fyler").open()
-			end)
+			L.setup_winbar()
+			vim.keymap.set("n", "S", L.open)
 		end,
 		config = function()
 			require("fyler").setup({
