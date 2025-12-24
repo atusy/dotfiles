70c2556cee264d2b340d63b17951a1059688d955
atusy <30277794+atusy@users.noreply.github.com>
2025-12-24 21:51:01 +0900

    feat!(fish): use osc52

--- dot_config/fish/functions/__abbr-yank.fish
+++ dot_config/fish/functions/__abbr-yank.fish
@@ -1,9 +1,3 @@
 function __abbr-yank
-  for cmd in pbcopy wl-copy
-    if type -q $cmd
-      echo -n $cmd
-      return
-    end
-  end
   echo 'base64 | xargs -I{} echo -n -e "\e]52;;{}\e\\\\"'
 end
