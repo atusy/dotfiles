e06c579c7abde6d426402454fbe8ee1041fd5a27
atusy <30277794+atusy@users.noreply.github.com>
2026-01-28 18:54:48 +0900

    feat(nvim): Prefer LSP semantic tokens over Treesitter for kakehashi

--- dot_config/nvim/after/lsp/kakehashi.lua
+++ dot_config/nvim/after/lsp/kakehashi.lua
@@ -3,6 +3,24 @@ if not home then
 	return
 end
 
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "Prefer LSP semantic tokens over Treesitter for kakehashi",
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if not client or client.name ~= "kakehashi" then
+			return
+		end
+
+		vim.api.nvim_create_autocmd("LspTokenUpdate", {
+			buffer = ev.buf,
+			once = true,
+			callback = function()
+				vim.treesitter.stop()
+			end,
+		})
+	end,
+})
+
 ---@type vim.lsp.Config
 return {
 	cmd = { vim.fs.joinpath(home, ".cargo/bin/kakehashi") },
