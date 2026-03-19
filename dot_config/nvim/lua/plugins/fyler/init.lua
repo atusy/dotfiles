a2664d8eddb39f4d2a33427e55c95c28400706b4
atusy <30277794+atusy@users.noreply.github.com>
2026-03-20 00:55:26 +0900

    feat(nvim): remove unused fyler setups

--- dot_config/nvim/lua/plugins/fyler/init.lua
+++ dot_config/nvim/lua/plugins/fyler/init.lua
@@ -1,55 +1,7 @@
-local function setup_winbar()
-	vim.api.nvim_create_autocmd("BufWinEnter", {
-		callback = function(ctx)
-			if vim.startswith(ctx.file, "fyler://") then
-				if not vim.w.old_winbar then
-					vim.w.old_winbar = vim.wo[0].winbar
-				end
-				vim.wo[0].winbar = "%F"
-				return
-			end
-
-			if vim.w.old_winbar then
-				vim.wo[0].winbar = vim.w.old_winbar
-			end
-		end,
-	})
-end
-
-local function open()
-	-- Move to an existing fyler window if one exists
-	local wins = vim.api.nvim_tabpage_list_wins(0)
-	for _, win in ipairs(wins) do
-		local buf = vim.api.nvim_win_get_buf(win)
-		local nm = vim.api.nvim_buf_get_name(buf)
-		if vim.startswith(nm, "fyler://") then
-			vim.api.nvim_set_current_win(win)
-			return
-		end
-	end
-
-	-- Open the existing fyler buffer if one exists
-	local bufs = vim.api.nvim_list_bufs()
-	for _, buf in ipairs(bufs) do
-		local nm = vim.api.nvim_buf_get_name(buf)
-		if vim.startswith(nm, "fyler://") then
-			vim.api.nvim_win_set_buf(0, buf)
-			return
-		end
-	end
-
-	-- Otherwise, open a new fyler window
-	if vim.v.count > 0 then
-		require("fyler").close()
-	end
-	require("fyler").open()
-end
-
 return {
 	{
 		"https://github.com/A7Lavinraj/fyler.nvim",
 		init = function()
-			setup_winbar()
 			vim.keymap.set("n", "S", function()
 				require("fyler").open()
 			end)
