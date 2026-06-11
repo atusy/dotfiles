bc1517726da4fd9fe295ab338ad9815b6d74a61d
atusy <30277794+atusy@users.noreply.github.com>
2026-06-11 22:20:43 +0900

    feat!(nvim): remove nvim-treesitter-endwise

--- dot_config/nvim/lua/plugins/insx/init.lua
+++ dot_config/nvim/lua/plugins/insx/init.lua
@@ -8,13 +8,4 @@ return {
 			require("plugins.insx.endwise").setup()
 		end,
 	},
-	{
-		-- to provide query files
-		"https://github.com/RRethy/nvim-treesitter-endwise",
-		init = function()
-			-- avoid unconditional run of require("nvim-treesitter-endwise").init() on source,
-			-- which sets a global <CR> on_key handler
-			package.loaded["nvim-treesitter-endwise"] = { init = function() end }
-		end,
-	},
 }
