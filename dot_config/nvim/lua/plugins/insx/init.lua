a6f8d144a61def33f2b3ed3d3b84733f2b3a54cc
atusy <30277794+atusy@users.noreply.github.com>
2026-06-11 22:20:07 +0900

    perf(nvim): watch endwise

--- dot_config/nvim/lua/plugins/insx/init.lua
+++ dot_config/nvim/lua/plugins/insx/init.lua
@@ -3,6 +3,7 @@ return {
 		"https://github.com/hrsh7th/nvim-insx",
 		event = "InsertEnter",
 		config = function()
+			require("kakehashi.extra.endwise").watch()
 			require("insx.preset.standard").setup()
 			require("plugins.insx.endwise").setup()
 		end,
