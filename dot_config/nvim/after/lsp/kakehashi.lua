2725365f9d93f4e18ae02b77c7f462564e4c1a87
atusy <30277794+atusy@users.noreply.github.com>
2026-07-06 20:08:44 +0900

    feat(nvim): let kakehashi binary be modifiable via env

--- dot_config/nvim/after/lsp/kakehashi.lua
+++ dot_config/nvim/after/lsp/kakehashi.lua
@@ -3,7 +3,10 @@ if not home then
 	return
 end
 
-local cmd = { vim.fs.joinpath(home, ".cargo/bin/kakehashi") }
+local KAKEHASHI_BIN = vim.env.KAKEHASHI_BIN or vim.fs.joinpath(home, ".cargo/bin/kakehashi")
+
+---@type string[]
+local cmd = { vim.uv.fs_stat(KAKEHASHI_BIN) and KAKEHASHI_BIN or "kakehashi" }
 
 for _, path in ipairs({
 	vim.fs.joinpath(home, "ghq/github.com/atusy/kakehashi-lspconfig/lsp.toml"),
