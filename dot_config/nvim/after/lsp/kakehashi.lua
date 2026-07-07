4c4e19ddece19a1dcbd6b7e13be70162193d0130
atusy <30277794+atusy@users.noreply.github.com>
2026-07-08 01:47:26 +0900

    refactor(nvim): define local function

--- dot_config/nvim/after/lsp/kakehashi.lua
+++ dot_config/nvim/after/lsp/kakehashi.lua
@@ -1,33 +1,37 @@
-local home = vim.uv.os_homedir()
-if not home then
-	return
-end
-
-local KAKEHASHI_BIN = vim.env.KAKEHASHI_BIN or vim.fs.joinpath(home, ".cargo/bin/kakehashi")
-
----@type string[]
-local cmd = { vim.uv.fs_stat(KAKEHASHI_BIN) and KAKEHASHI_BIN or "kakehashi" }
-
-for _, path in ipairs({
-	vim.fs.joinpath(home, "ghq/github.com/atusy/kakehashi-lspconfig/lsp.toml"),
-	vim.fs.joinpath(home, ".config/kakehashi/kakehashi.toml"),
-	"kakehashi.toml",
-}) do
-	if vim.uv.fs_stat(path) then
-		table.insert(cmd, "--config-file")
-		table.insert(cmd, path)
+local function gen_cmd()
+	local home = vim.uv.os_homedir()
+	if not home then
+		return { "kakehashi" }
 	end
-end
 
----@type string[]
-local extra_args = vim.json.decode(vim.env.KAKEHASHI_EXTRA_ARGS or "[]")
-for _, arg in ipairs(extra_args) do
-	table.insert(cmd, arg)
+	local KAKEHASHI_BIN = vim.env.KAKEHASHI_BIN or vim.fs.joinpath(home, ".cargo/bin/kakehashi")
+
+	---@type string[]
+	local cmd = { vim.uv.fs_stat(KAKEHASHI_BIN) and KAKEHASHI_BIN or "kakehashi" }
+
+	for _, path in ipairs({
+		vim.fs.joinpath(home, "ghq/github.com/atusy/kakehashi-lspconfig/lsp.toml"),
+		vim.fs.joinpath(home, ".config/kakehashi/kakehashi.toml"),
+		"kakehashi.toml",
+	}) do
+		if vim.uv.fs_stat(path) then
+			table.insert(cmd, "--config-file")
+			table.insert(cmd, path)
+		end
+	end
+
+	---@type string[]
+	local extra_args = vim.json.decode(vim.env.KAKEHASHI_EXTRA_ARGS or "[]")
+	for _, arg in ipairs(extra_args) do
+		table.insert(cmd, arg)
+	end
+
+	return cmd
 end
 
 ---@type vim.lsp.Config
 return {
-	cmd = cmd,
+	cmd = gen_cmd(),
 	cmd_env = {
 		KAKEHASHI_EXPERIMENTAL = vim.env.KAKEHASHI_EXPERIMENTAL or "true",
 	},
