669d1b2fa4764dfca2a2dfc8c462001a3b69cbde
atusy <30277794+atusy@users.noreply.github.com>
2026-07-24 00:28:10 +0900

    feat(nvim): use native deno for denops

--- dot_config/nvim/lua/plugins/denops/init.lua
+++ dot_config/nvim/lua/plugins/denops/init.lua
@@ -2,9 +2,9 @@ return {
 	{
 		"https://github.com/vim-denops/denops.vim",
 		init = function(p)
-			local bin, cache = require("plugins.denops.utils").get_deno("2.8.3")
-			vim.g["denops#deno"] = bin
-			vim.g["denops#deno_dir"] = cache
+			-- local bin, cache = require("plugins.denops.utils").get_deno("2.8.3")
+			-- vim.g["denops#deno"] = bin
+			-- vim.g["denops#deno_dir"] = cache
 			vim.g["denops#server#deno_args"] = { "-q", "--no-lock", "-A", "--unstable-kv" }
 		end,
 	},
