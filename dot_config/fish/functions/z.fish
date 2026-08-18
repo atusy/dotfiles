b688081e3ef8bc7ea71f90a016347734dbf6857d
atusy <30277794+atusy@users.noreply.github.com>
2026-08-19 08:54:57 +0900

    feat(fish): make z pipe friendly

--- dot_config/fish/functions/z.fish
+++ dot_config/fish/functions/z.fish
@@ -1,19 +1,26 @@
-function __zi
-  begin
-    zoxide query --list
-    ghq list -p
-    find "$HOME/.local/share/nvim/lazy" -maxdepth 1 -type d
-  end \
-    | perl -ne 'print unless $seen{$_}++' \
-    | fzf --layout=reverse --no-sort --height=~15 $argv \
-    | read -l result
-  and __zoxide_cd $result
+function __zi_query
+    begin
+        zoxide query --list
+        ghq list -p
+        find "$HOME/.local/share/nvim/lazy" -maxdepth 1 -type d
+    end \
+        | perl -ne 'print unless $seen{$_}++' \
+        | fzf --layout=reverse --no-sort --height=~15 $argv
 end
 
 function z --description 'zoxide wrapper'
-  if test (count $argv) -eq 1
-    __zoxide_z $argv || true
-  else
-    __zi --query="$argv"
-  end
+    if test (count $argv) -eq 1
+        if test -t 1
+            __zoxide_z $argv || true
+        else
+            zoxide query $argv
+        end
+    else
+        set -l selected (__zi_query --query="$argv")
+        if test -t 1
+            __zoxide_cd "$selected"
+        else
+            echo "$selected"
+        end
+    end
 end
